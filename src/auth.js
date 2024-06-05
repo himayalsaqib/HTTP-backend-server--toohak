590908c42f9f56c0be129e6860c080e97fb3f0e5
Himayal Saqib <z5534127@ad.unsw.edu.au>
2024-06-05 10:55:42 +1000

    Added stub for adminUserPasswordUpdate function and wrote the function documentation for it

---
+++ src/auth.js
@@ -0,0 +1,12 @@
+/**
+ * Given details relating to a password change, update the password 
+ * of a logged in user.
+ *
+ * @param {number} authUserId
+ * @param {string} oldPassword
+ * @param {string} newPassword
+ * @returns {empty}
+ */
+function adminUserPasswordUpdate(authUserId, oldPassword, newPassword) {
+  return {};
+}
