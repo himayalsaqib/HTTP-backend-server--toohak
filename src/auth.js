2548538af351fa15e19f32cd0f60640024598e7c
Sarah Townsley <z5481287@ad.unsw.edu.au>
2024-06-05 13:44:41 +1000

    created function stub for adminUserDetails + documentation

--- src/auth.js
+++ src/auth.js
@@ -17,3 +17,19 @@ function adminAuthLogin (email, password) {
         authUserId: 1,
     };
 }
+
+// Given an admin user's authUserId, return details about the user.
+// "name" is the first and last name concatenated with a single space 
+// between them.
+
+function adminUserDetails ( authUserId ) {
+    return { user:
+        {
+          userId: 1,
+          name: 'Hayden Smith',
+          email: 'hayden.smith@unsw.edu.au',
+          numSuccessfulLogins: 3,
+          numFailedPasswordsSinceLastLogin: 1,
+        }
+      };
+}
