f14b8679c67468e06d76a9aa6a9525d279a41df2
Haleemah Rahman <z5361900@ad.unsw.edu.au>
2024-06-05 10:30:23 +1000

    Created stub function adminAuthRegister

---
+++ src/auth.js
@@ -0,0 +1,9 @@
+// Register a user with an email, password, and names, then returns their authUserId value
+// Parameters: email, password, nameFirst, nameLast
+// Returns authUserId object 
+
+function adminAuthRegister (email, password, nameFirst, nameLast) {
+    return {
+        authUserId: 1,
+    };
+}
