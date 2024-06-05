fbeca70ed2072591e2c3b089d988fbe802a0bce4
Sameera Zaman <z5361907@ad.unsw.edu.au>
2024-06-05 19:51:29 +1000

    Stubbing the function adminQuizInfo to quiz.js

--- src/quiz.js
+++ src/quiz.js
@@ -50,3 +50,21 @@ function adminQuizRemove ( authUserId, quizId ) {
 function adminQuizDescriptionUpdate (authUserId, quizId, description) {
     return {}
 }
+
+/**
+ * Provide a list of all quizzes that are owned by the 
+ * currently logged in user.
+ * 
+ * @param {number} authUserId
+ * @param {number} quizId 
+ * @returns {{quizId: number, name: string, timeCreated: number, timeLastEdited: number, description: string}}
+ */
+function adminQuizInfo (authUserId, quizId) {
+    return {
+        quizId: 1,
+        name: 'My Quiz',
+        timeCreated: 1683125870,
+        timeLastEdited: 1683125871,
+        description: 'This is my quiz',
+    }
+  }
