e6b19c99927e0be122ae9f9e6f9e4be8cfa46f57
Sameera Zaman <z5361907@ad.unsw.edu.au>
2024-06-05 18:22:26 +1000

    Deleted adminQuizInfoFunction from auth.js

--- src/auth.js
+++ src/auth.js
@@ -65,21 +65,3 @@ function adminUserDetailsUpdate( authUserId, email, nameFirst, nameLast ) {
   return {};
 }
 
-/**
- * Provide a list of all quizzes that are owned by the 
- * currently logged in user.
- * 
- * @param {number} authUserId
- * @param {number} quizId 
- * @returns {{quizId: number, name: string, timeCreated: number, timeLastEdited: number, description: string}}
- */
-function adminQuizInfo (authUserId, quizId) {
-  return {
-      quizId: 1,
-      name: 'My Quiz',
-      timeCreated: 1683125870,
-      timeLastEdited: 1683125871,
-      description: 'This is my quiz',
-  }
-}
-
