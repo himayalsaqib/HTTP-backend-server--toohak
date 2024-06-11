584b357bbe99eb51f51bb2a32da8d4db0c260ad8
Sarah Townsley <z5481287@ad.unsw.edu.au>
2024-06-11 21:10:34 +1000

    registers a user with adminAuthRegister to use ID for testing and changed userId to authUserId

--- src/quiz/adminCreateQuiz.test.js
+++ src/quiz/adminCreateQuiz.test.js
@@ -5,48 +5,40 @@ import {clear} from '../other';
 
 beforeEach(() => {
     clear();
-    let users = [{nameFirst: "validFirstName", nameLast: "validLastName", 
-                password: "validPassword1", userId: 1, 
-                email: "validEmail@gmail.com", numFailedLogins: 0, 
-                numSuccessfulLogins: 0
-    }];
-    let currentUser = users[0];
-
-    let quizzes = [{quizId: 1, name: "Name In Use", timeCreated: 1, 
-                timeLastEdited: 1, 
-                description: "Pre existing quiz description"
-    }];
+    const user = adminAuthRegister('valid1@gmail.com', 'Password12', 'Jane', 'Doe');
 });
 
 describe('adminCreateQuiz', () => {
     test('Test: successful adminCreateQuiz', () => {
-        expect(adminCreateQuiz(currentUser.userId, "Valid Quiz Name", 
+        expect(adminCreateQuiz(user.authUserId, "Valid Quiz Name", 
         "Valid quiz description.")).toStrictEqual(expect.any(Number));
     });
 
     test('Test: invalid user ID', () => {
-        expect(adminCreateQuiz(0, "Valid Quiz Name", 
+        expect(adminCreateQuiz(-1, "Valid Quiz Name", 
         "Valid quiz description.")).toStrictEqual({ error: expect.any(string) });
     });
 
     test('Test: quiz name contains invalid characters', () => {
-        expect(adminCreateQuiz(currentUser.userId, "Invalid Name @#$%^&*", 
+        expect(adminCreateQuiz(user.authUserId, "Invalid Name @#$%^&*", 
         "Valid quiz description.")).toStrictEqual({ error: expect.any(string) });
     });
 
     test('Test: quiz name is less than 3 characters', () => {
-        expect(adminCreateQuiz(currentUser.userId, "Hi", 
+        expect(adminCreateQuiz(user.authUserId, "Hi", 
         "Valid quiz description.")).toStrictEqual({ error: expect.any(string) });
     });
 
     test('Test: quiz name is more than 30 characters', () => {
-        expect(adminCreateQuiz(currentUser.userId, 
+        expect(adminCreateQuiz(user.authUserId, 
             "1234567890 1234567890 1234567890",
              "Valid quiz description.")).toStrictEqual({ error: expect.any(string) });
     });
 
     test('Test: quiz name already used by current user for another quiz', () => {
-        expect(adminCreateQuiz(currentUser.userId, "Name In Use", 
+        adminCreateQuiz(user.Id, "Name In Use", "Pre existing quiz");
+
+        expect(adminCreateQuiz(user.authUserId, "Name In Use", 
         "Valid quiz description.")).toStrictEqual({ error: expect.any(string) });
     });
 
@@ -54,7 +46,7 @@ describe('adminCreateQuiz', () => {
                     1234567890 1234567890 1234567890 1234567890 1234567890"
 
     test('Test: quiz description is more than 100 characters', () => {
-        expect(adminCreateQuiz(currentUser.userId, "Valid Quiz Name", 
+        expect(adminCreateQuiz(user.authUserId, "Valid Quiz Name", 
         longString)).toStrictEqual({ error: expect.any(string) });
     });
 });
