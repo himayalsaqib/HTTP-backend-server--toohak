34ecdcdfe2e7f34baf5512295a99b71c4e1a213f
Tara Moore <z5487977@ad.unsw.edu.au>
2024-06-11 14:44:37 +1000

    wrote another test for clear() which checks successful deletion of a user

--- src/other.test.js
+++ src/other.test.js
@@ -1,11 +1,21 @@
-
 import { clear } from './other';
+import {
+    adminAuthRegister,
+    adminAuthLogin,
+    adminUserDetails,
+    adminUserDetailsUpdate,
+    adminUserPasswordUpdate
+} from './auth';
 
 
 describe('clear function', () =>{
-    test('has one return type', () => {
+    test('has correct return type', () => {
         expect(clear()).toStrictEqual({});
     });
 
-    test 
+    test('clear successful', () => {
+        const idTest = adminAuthRegister('email@email.com', 'password', 'nameFirst', 'nameLast');
+        clear();
+        expect(adminUserDetails(idTest)).toStrictEqual({ error : expect.any(String)});
+    });
 });
