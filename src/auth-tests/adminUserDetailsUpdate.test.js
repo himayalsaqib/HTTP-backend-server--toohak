1df904578d11aedd32414b3236993ce184d56320
Sarah Townsley <z5481287@ad.unsw.edu.au>
2024-06-15 16:57:02 +1000

    added tests for other invalid input

--- src/auth-tests/adminUserDetailsUpdate.test.js
+++ src/auth-tests/adminUserDetailsUpdate.test.js
@@ -24,5 +24,42 @@ describe('adminQuizCreate', () => {
         'Jane', 'Doe')).toStrictEqual( error );
     });
 
-    
+    test('Returns error when email is not valid', () => {
+        expect(adminUserDetailsUpdate(user.authUserId, 'invalid1.com', 
+        'Jane', 'Doe')).toStrictEqual( error );
+    });
+
+    test('Returns error when nameFirst contains invalid characters', () => {
+        expect(adminUserDetailsUpdate(user.authUserId, 'valid1@gmail.com', 
+        'Jane1', 'Doe')).toStrictEqual( error );
+        expect(adminUserDetailsUpdate(user.authUserId, 'valid1@gmail.com', 
+        'Jane&', 'Doe')).toStrictEqual( error );
+    });
+
+    test('Returns error when nameFirst is less than 2 characters', () => {
+        expect(adminUserDetailsUpdate(user.authUserId, 'valid1@gmail.com', 
+        'J', 'Doe')).toStrictEqual( error );
+    });
+
+    test('Returns error when nameFirst is more than 20 characters', () => {
+        expect(adminUserDetailsUpdate(user.authUserId, 'valid1@gmail.com', 
+        'JamieJamieJamieJamieJamie', 'Doe')).toStrictEqual( error );
+    });
+
+    test('Returns error when nameLast contains invalid characters', () => {
+        expect(adminUserDetailsUpdate(user.authUserId, 'valid1@gmail.com', 
+        'Jane', 'Doe1')).toStrictEqual( error );
+        expect(adminUserDetailsUpdate(user.authUserId, 'valid1@gmail.com', 
+        'Jane', 'Doe&')).toStrictEqual( error );
+    });
+
+    test('Returns error when nameLast is less than 2 characters', () => {
+        expect(adminUserDetailsUpdate(user.authUserId, 'valid1@gmail.com', 
+        'Jane', 'D')).toStrictEqual( error );
+    });
+
+    test('Returns error when nameLast is more than 20 characters', () => {
+        expect(adminUserDetailsUpdate(user.authUserId, 'valid1@gmail.com', 
+        'Jane', 'DavidDavidDavidDavidDavid')).toStrictEqual( error );
+    });
 });
