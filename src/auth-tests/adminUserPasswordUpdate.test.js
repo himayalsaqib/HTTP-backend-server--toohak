ea1306f2385d19268af36e81e6f6bf2e7ed49def
Tara Moore <z5487977@ad.unsw.edu.au>
2024-06-16 15:17:50 +1000

    expanded test to-do list

--- src/auth-tests/adminUserPasswordUpdate.test.js
+++ src/auth-tests/adminUserPasswordUpdate.test.js
@@ -1,4 +1,4 @@
-import { adminUserPasswordUpdate } from "../auth";
+import { adminUserPasswordUpdate, adminAuthRegister, adminAuthLogin } from "../auth";
 import { clear } from "../other";
 
 const ERROR = { error : expect.any(String) };
@@ -7,4 +7,14 @@ beforeEach(() => {
     clear();
 });
 
-test.todo('')
+test.todo('when authUserId is not a valid user, from adminAuthRegister & adminAuthLogin');
+
+test.todo('old password is not the correct old password');
+
+test.todo('old password and new password match exactly');
+
+test.todo('new password has already been used before by the user');
+
+test.todo('new password is less than 8 characters');
+
+test.todo('new password does not contain at least one number and at least one letter');
