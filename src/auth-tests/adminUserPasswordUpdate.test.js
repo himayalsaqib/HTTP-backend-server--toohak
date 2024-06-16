b6632711a14ec537c14066eb41ca14860c0d7a29
Tara Moore <z5487977@ad.unsw.edu.au>
2024-06-16 15:07:32 +1000

    created test file in auth-tests, imported inital functions, defined error object, started to-do lists for tests

---
+++ src/auth-tests/adminUserPasswordUpdate.test.js
@@ -0,0 +1,10 @@
+import { adminUserPasswordUpdate } from "../auth";
+import { clear } from "../other";
+
+const ERROR = { error : expect.any(String) };
+
+beforeEach(() => {
+    clear();
+});
+
+test.todo('')
