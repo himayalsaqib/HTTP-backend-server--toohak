16ec03281266de8f0e05ed426cecafbb4c138d3c
Tara Moore <z5487977@ad.unsw.edu.au>
2024-06-18 15:34:42 +1000

    made updating password code more simple by removing unneccesary for-loop

--- src/auth.js
+++ src/auth.js
@@ -174,10 +174,10 @@ export function adminUserPasswordUpdate(authUserId, oldPassword, newPassword) {
   // update password for user
   for (const user of data.users) {
     if (user.authUserId === authUserId) {
-      for (const password in user.previousPasswords) {
-        password.password = newPassword;
-        password.previousPasswords.push(oldPassword);
-      }
+      user.password = newPassword;
+      //for (const password in user.previousPasswords) {
+      user.previousPasswords.push(oldPassword);
+      //}
     }
   }
 
