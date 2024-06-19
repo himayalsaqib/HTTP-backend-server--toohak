d70857e44a111e508a3498f8827f164f490e05d0
Tara Moore <z5487977@ad.unsw.edu.au>
2024-06-19 10:08:47 +1000

    moved clear.test.js to other-test directory, as was recommended for organisation of the project

---
+++ src/other-tests/clear.test.js
@@ -0,0 +1,47 @@
+import { clear } from "../other";
+import {
+    adminAuthRegister,
+    adminAuthLogin,
+    adminUserDetails
+} from "../auth";
+
+
+describe('clear', () =>{
+    describe('test for the return type of clear', () => {
+        test('has correct return type', () => {
+            expect(clear()).toStrictEqual({});
+        });
+    });
+    
+
+    describe('testing side-effects of clear', () => {
+        let user;
+        beforeEach(() => {
+            user = adminAuthRegister('email@gmail.com', 'password123', 'Jane', 'Doe');
+        });
+        
+        test('successful registration of user via logging in', () => {
+
+        });
+
+        test('successful access of user details in adminUserDetails', () => {
+            let newUser = adminAuthRegister('valid@gmail.com', 'validpa55word', 'John', 'Smith');
+            expect(adminAuthLogin('valid@gmail.com', 'validpa55word')).toStrictEqual({ authUserId : newUser.authUserId });
+            
+            expect(adminUserDetails(newUser.authUserId)).toStrictEqual({
+                user: {
+                    userId: newUser.authUserId,
+                    name: 'John Smith',
+                    email: 'valid@gmail.com',
+                    numSuccessfulLogins: 2,
+                    numFailedPasswordsSinceLastLogin: 0,
+                }
+            });
+        });
+        
+        test('error returned by adminUserDetails after calling clear', () => {
+            clear();
+            expect(adminUserDetails(user.authUserId)).toStrictEqual({ error : expect.any(String) });
+        });
+    });
+});
