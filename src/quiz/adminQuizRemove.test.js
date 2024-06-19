bdc9ffbe8797fcd4a454e7bb1758fb9c6b79534b
Haleemah Rahman <z5361900@ad.unsw.edu.au>
2024-06-19 15:18:09 +1000

    Fixed up all the tests so that it correctly accesses authUserId and quizId by using dot notation. Got rid of extra calls of adminQuizRemove within the remove multiple quizzes test

--- src/quiz/adminQuizRemove.test.js
+++ src/quiz/adminQuizRemove.test.js
@@ -8,11 +8,6 @@ beforeEach(()=> {
     clear();
 });
 
-describe('clear', () => {
-    test('has the correct return type, {}', () => {
-      expect(clear()).toStrictEqual({});
-    });
-  });
 
 describe('adminQuizRemove', () => {
 
@@ -25,26 +20,27 @@ describe('adminQuizRemove', () => {
 
     describe('Remove quiz from invalid ids returns error', () => {
         test('Invalid authUserId, valid quizId', () => {
-            const invalidUser = -1;
-            const quiz = adminQuizRemove(invalidUserId, 1);
-
-            expect(quiz.strictEqual({error: 'Invalid User Id'}));
+            const invalidUserId = -1;
+            const user = adminAuthRegister('user1@gmail.com', 'Password01', 'User', 'One');
+            const quizId = adminQuizCreate(user.authUserId, 'Quiz 1', 'Description 1').quizId;
+            const quiz = adminQuizRemove(invalidUserId, quizId);
+            expect(quiz).toStrictEqual({error: 'AuthUserId does not refer to a valid user id.'});
         });
 
-        test('Valid user, invalid quizId', () => {
+        test('Valid authUserId, invalid quizId', () => {
             const user = adminAuthRegister('user@gmail.com', 'Password01', 'First', 'Last');
-            const invalidQuiz = -1;
+            const invalidQuizId = -1;
 
-            expect(adminQuizRemove(user.authUserId, invalidQuizId).
-            toStrictEqual({error: 'Invalid Quiz Id'}));
+            expect(adminQuizRemove(user.authUserId, invalidQuizId)).
+            toStrictEqual({error: 'Quiz Id does not refer to a valid quiz.'});
         });
 
-        test('Invalid user, invalid quizId', () => {
+        test('Invalid authUserId, invalid quizId', () => {
             const invalidUser = -1
-            const invalidQuiz = -1;
+            const invalidQuizId = -1;
 
-            expect(adminQuizRemove(invalidUser, invalidQuiz).
-            toStrictEqual({error: 'Invalid User and Quiz Id'}));
+            expect(adminQuizRemove(invalidUser, invalidQuizId)).
+            toStrictEqual({error: 'Invalid User and Quiz Id'});
         });
 
     });
@@ -52,12 +48,10 @@ describe('adminQuizRemove', () => {
 
     describe('Quiz does not belong to user', () => {
         test('should not remove valid quiz that does not belong to user ', () => {
-            const user1 = adminAuthRegister('user1@gmail.com', 'Password01', 'User', 'One');
-            const user2 = adminAuthRegister('user2@gmail.com', 'Password02', 'User', 'Two');
-            const quiz1 = adminQuizCreate(user1.authUserId, 'Quiz 1', 'Description 1');
-
-            expect(adminQuizRemove(user2, quiz1.quizId)).
-            toStrictEqual({error: 'Quiz does not belong to user'});
+            const user1 = adminAuthRegister('user1@gmail.com', 'Password01', 'User', 'One').authUserId;
+            const user2 = adminAuthRegister('user2@gmail.com', 'Password02', 'User', 'Two').authUserId;
+            const quiz1 = adminQuizCreate(user1, 'Quiz 1', 'Description 1').quizId;
+            expect(adminQuizRemove(user2, quiz1)).toStrictEqual({error: 'Quiz does not belong to user'});
         }); 
     });
 
@@ -65,32 +59,40 @@ describe('adminQuizRemove', () => {
     describe('Successful adminQuizRemove', () => {
 
         test('Successfully remove a quiz', () => {
-            const user = adminAuthRegister('user@gmail.com', 'Password01', 'userfirst', 'userlast');
-            const quiz = adminQuizCreate(user.authUserId, 'Quiz', 'Description');
+            const user = adminAuthRegister('user@gmail.com', 'Password01', 'userfirst', 'userlast').authUserId;
+            const quiz = adminQuizCreate(user, 'Quiz', 'Description').quizId;
 
-            expect(adminQuizRemove(user.authUserId, quiz.quizId)).toStrictEqual({});
+            expect(adminQuizRemove(user, quiz)).toStrictEqual({});
         });
 
         test('Remove a quiz that has already been successfully removed', () => {
-            const user = adminAuthRegister('user@gmail.com', 'Password01', 'userfirst', 'userlast');
-            const quiz = adminQuizCreate(user.authUserId, 'Quiz', 'Description');
+            // idk how to implement this 
+            // how to differentiate between quizIdExists(quizId) === false condition
+            // and a quiz that existed but got removed
+            const user = adminAuthRegister('user@gmail.com', 'Password01', 'userfirst', 'userlast').authUserId;
+            const quiz = adminQuizCreate(user, 'Quiz', 'Description').quizId;
 
-            adminQuizRemove(user.authUserId, quiz.quizId);
+            adminQuizRemove(user, quiz);
 
-            expect(adminQuizRemove(user.authUserId, quiz.quizId)).
+            expect(adminQuizRemove(user, quiz)).
             toStrictEqual({error: "Quiz does not exist"});
         });
 
         test('Remove mulitple quizzes', () => {
-            const user = adminAuthRegister('user@gmail.com', 'Password01', 'userfirst', 'userlast');
-            const quiz01 = adminQuizCreate(user.authUserId, 'Quiz 01', 'Description 01');
-            const quiz02 = adminQuizCreate(user.authUserId, 'Quiz 02', 'Description 02');
+            const user = adminAuthRegister('user@gmail.com', 'Password01', 'userfirst', 'userlast').authUserId;
+            const quiz01 = adminQuizCreate(user, 'Quiz 01', 'Description 01').quizId;
+            const quiz02 = adminQuizCreate(user, 'Quiz 02', 'Description 02').quizId;
 
-            adminQuizRemove(user.authUserId, quiz01.quizId);
-            adminQuizRemove(user.authUserId, quiz02.quizId);
+            // adminQuizRemove(user, quiz01);
+            //expect(adminQuizRemove(user, quiz01)).toStrictEqual({});
+            // adminQuizRemove(user, quiz02);
 
-            expect(adminQuizRemove(user.authUserId, quiz01.quizId)).toStrictEqual({});
-            expect(adminQuizRemove(user.authUserId, quiz02.quizId)).toStrictEqual({});
+            expect(adminQuizRemove(user, quiz01)).toStrictEqual({});
+            // shoud it print the remaining quiz02
+            expect(adminQuizRemove(user, quiz02)).toStrictEqual({});
+
+            // expect(user, quiz01).toStrictEqual({});
+            // expect(user, quiz02).toStrictEqual({});
         });
 
     });
