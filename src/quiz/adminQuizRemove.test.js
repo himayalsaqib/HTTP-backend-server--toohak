b40174bddb3cbf409008f7da3423d5c8987adce5
Haleemah Rahman <z5361900@ad.unsw.edu.au>
2024-06-19 21:54:45 +1000

    Removed whitespace just after clear

---
+++ src/quiz/adminQuizRemove.test.js
@@ -0,0 +1,82 @@
+// tests for adminQuizRemove function 
+
+import { adminQuizCreate, adminQuizRemove } from '../quiz';
+import { adminAuthRegister } from '../auth';
+import { clear } from '../other';
+
+beforeEach(()=> {
+    clear();
+});
+
+describe('adminQuizRemove', () => {
+
+  describe('Has the correct return type', () => {
+    test('Successfully remove a quiz', () => {
+        const user = adminAuthRegister('user@gmail.com', 'Password01', 'userfirst', 'userlast').authUserId;
+        const quiz = adminQuizCreate(user, 'Quiz', 'Description').quizId;
+
+        expect(adminQuizRemove(user, quiz)).toStrictEqual({});
+        //expect(adminQuizInfo(user, quiz)).ToStrictEqual({error: expecy.any(String)});
+    });
+
+    test('Remove mulitple quizzes', () => {
+        const user = adminAuthRegister('user@gmail.com', 'Password01', 'userfirst', 'userlast').authUserId;
+        const quiz01 = adminQuizCreate(user, 'Quiz 01', 'Description 01').quizId;
+        const quiz02 = adminQuizCreate(user, 'Quiz 02', 'Description 02').quizId;
+
+        expect(adminQuizRemove(user, quiz01)).toStrictEqual({});
+        expect(adminQuizRemove(user, quiz02)).toStrictEqual({});
+    });
+
+});
+
+    describe('Remove quiz from invalid ids returns error', () => {
+        test('Invalid authUserId, valid quizId', () => {
+            const invalidUserId = -1;
+            const user = adminAuthRegister('user1@gmail.com', 'Password01', 'User', 'One');
+            const quizId = adminQuizCreate(user.authUserId, 'Quiz 1', 'Description 1').quizId;
+            const quiz = adminQuizRemove(invalidUserId, quizId);
+            
+            expect(quiz).toStrictEqual({error: expect.any(String)});
+        });
+
+        test('Valid authUserId, invalid quizId', () => {
+            const user = adminAuthRegister('user@gmail.com', 'Password01', 'First', 'Last');
+            const invalidQuizId = -1;
+
+            expect(adminQuizRemove(user.authUserId, invalidQuizId)).
+            toStrictEqual({error: expect.any(String)});
+        });
+
+        test('Invalid authUserId, invalid quizId', () => {
+            const invalidUser = -1
+            const invalidQuizId = -1;
+
+            expect(adminQuizRemove(invalidUser, invalidQuizId)).
+            toStrictEqual({error: expect.any(String)});
+        });
+
+    });
+    
+
+    describe('Quiz does not belong to user', () => {
+        test('should not remove valid quiz that does not belong to user ', () => {
+            const user1 = adminAuthRegister('user1@gmail.com', 'Password01', 'User', 'One').authUserId;
+            const user2 = adminAuthRegister('user2@gmail.com', 'Password02', 'User', 'Two').authUserId;
+            const quiz1 = adminQuizCreate(user1, 'Quiz 1', 'Description 1').quizId;
+            
+            expect(adminQuizRemove(user2, quiz1)).toStrictEqual({error: expect.any(String)});
+        }); 
+
+        test('Remove a quiz that has already been successfully removed', () => {
+            const user = adminAuthRegister('user@gmail.com', 'Password01', 'userfirst', 'userlast').authUserId;
+            const quiz = adminQuizCreate(user, 'Quiz', 'Description').quizId;
+
+            adminQuizRemove(user, quiz);
+
+            expect(adminQuizRemove(user, quiz)).
+            toStrictEqual({error: expect.any(String)});
+        });
+    });
+
+});
