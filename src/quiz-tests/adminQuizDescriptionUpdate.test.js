c819eea53d225da708f35dd9403ee1da1f146d19
Sameera Zaman <z5361907@ad.unsw.edu.au>
2024-06-20 21:14:18 +1000

    Moved global variables under describe and fixed up tab to 2

--- src/quiz-tests/adminQuizDescriptionUpdate.test.js
+++ src/quiz-tests/adminQuizDescriptionUpdate.test.js
@@ -2,18 +2,18 @@ import { adminAuthRegister } from '../auth';
 import { adminQuizCreate, adminQuizDescriptionUpdate } from '../quiz';
 import { clear } from '../other';
 
-let user;
-let quizId;
-const error = { error: expect.any(String) };
-
-beforeEach(() => {
-  clear();
-  user = adminAuthRegister('valid1@gmail.com', 'Password12', 'Jane', 'Doe');
-  const quiz = adminQuizCreate(user.authUserId, 'OG Quiz Name', 'Valid quiz description');
-  quizId = quiz.quizId;
-});
-
 describe('adminQuizDescriptionUpdate', () => {
+  let user;
+  let quizId;
+  const error = { error: expect.any(String) };
+
+  beforeEach(() => {
+    clear();
+    user = adminAuthRegister('valid1@gmail.com', 'Password12', 'Jane', 'Doe');
+    const quiz = adminQuizCreate(user.authUserId, 'OG Quiz Name', 'Valid quiz description');
+    quizId = quiz.quizId;
+  });
+
   describe('Testing for correct return type', () => {
     test('Update quiz description successfully', () => {
       expect(adminQuizDescriptionUpdate(user.authUserId, quizId, 'New quiz description')).not.toStrictEqual(error);
@@ -42,7 +42,6 @@ describe('adminQuizDescriptionUpdate', () => {
       const longDescription = 'Okay deep breath. This is going to be a really really really long quiz description. Like really long. Absurdly inconviently long.';
       expect(adminQuizDescriptionUpdate(user.authUserId, quizId, longDescription)).toStrictEqual(error);
     });
-  
     test('Description exactly 100 characters', () => {
       const description = "This description is exactly 100 characters long and it's used to test the edge case of the function.";
       expect(adminQuizDescriptionUpdate(user.authUserId, quizId, description)).not.toStrictEqual(error);
