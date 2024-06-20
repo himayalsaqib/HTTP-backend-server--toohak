b312aab9bf3b8a70d59bb47a882879692936926e
Sameera Zaman <z5361907@ad.unsw.edu.au>
2024-06-20 19:39:10 +1000

    Changed tab size to 2 and formatted the tests so they are consistent with other tests (added more describes to make it easier to follow and commented out updated description due to that function not being implemented yet

--- src/quiz-tests/adminQuizInfo.test.js
+++ src/quiz-tests/adminQuizInfo.test.js
@@ -4,12 +4,6 @@ import { adminAuthRegister } from '../auth';
 import { adminQuizCreate, adminQuizDescriptionUpdate, adminQuizInfo, adminQuizNameUpdate } from '../quiz';
 import { clear } from '../other';
 
-
-
-
-// add a test case for if quiz was updated and if quiz was not updated; to Test after the implementation of adminNameUpdate,etc
-
-
 describe('adminQuizInfo', () => {
 	let user; 
 	let quizId;
@@ -21,55 +15,59 @@ describe('adminQuizInfo', () => {
 		const quiz = adminQuizCreate(user.authUserId, 'Valid Quiz Name', 'Valid quiz description');
 		quizId = quiz.quizId;
 	});
-    
+
+	describe('Testing for correct return type', () => {
     test('Quiz info with updated name was successful and has correct return type', () => {
-        adminQuizNameUpdate(user.authUserId, quizId, 'Updated Quiz Name');
-        expect(adminQuizInfo(user.authUserId, quizId)).toStrictEqual({
-            quizId: quizId,
-            name: 'Updated Quiz Name',
-            timeCreated: expect.any(Number),
-            timeLastEdited: expect.any(Number),
-            description: 'Valid quiz description'
-        });
-    });
+			adminQuizNameUpdate(user.authUserId, quizId, 'Updated Quiz Name');
+			expect(adminQuizInfo(user.authUserId, quizId)).toStrictEqual({
+				quizId: quizId,
+				name: 'Updated Quiz Name',
+				timeCreated: expect.any(Number),
+				timeLastEdited: expect.any(Number),
+				description: 'Valid quiz description'
+			});
+		});
+		/*
     test('Quiz info with updated description was successful and has correct return type', () => {
-        adminQuizDescriptionUpdate(user.authUserId, quizId, 'Updated quiz description');
-        expect(adminQuizInfo(user.authUserId, quizId)).toStrictEqual({
-            quizId: quizId,
-            name: 'Valid Quiz Name',
-            timeCreated: expect.any(Number),
-            timeLastEdited: expect.any(Number),
-            description: 'Updated quiz description'
-        });
-    });
-    
-
+			adminQuizDescriptionUpdate(user.authUserId, quizId, 'Updated quiz description');
+			expect(adminQuizInfo(user.authUserId, quizId)).toStrictEqual({
+				quizId: quizId,
+				name: 'Valid Quiz Name',
+				timeCreated: expect.any(Number),
+				timeLastEdited: expect.any(Number),
+				description: 'Updated quiz description'
+			});
+		});
+		*/
     test('Quiz info for new quiz was successful and has correct return type', () => {
-        expect(adminQuizInfo(user.authUserId, quizId)).toStrictEqual({
-            quizId: quizId,
-            name: 'Valid Quiz Name',
-            timeCreated: expect.any(Number),
-            timeLastEdited: undefined,
-            description: 'Valid quiz description'
-        });
-    });
+			expect(adminQuizInfo(user.authUserId, quizId)).toStrictEqual({
+				quizId: quizId,
+				name: 'Valid Quiz Name',
+				timeCreated: expect.any(Number),
+				timeLastEdited: undefined,
+				description: 'Valid quiz description'
+			});
+		});
+	});
 
+	describe('Testing authUserId in adminQuizInfo', () => {
     test('Invalid authUserId', () => {
-        expect(adminQuizInfo('invalidUser123', quizId)).toStrictEqual(error);
+			expect(adminQuizInfo('invalidUser123', quizId)).toStrictEqual(error);
     });
+	});
 
+	describe('Testing quizId in adminQuizInfo', () => {
     test('Invalid quizId', () => {
-        expect(adminQuizInfo(user.authUserId, 'invalidQuiz123')).toStrictEqual(error);
+			expect(adminQuizInfo(user.authUserId, 'invalidQuiz123')).toStrictEqual(error);
     });
-
     test('Quiz not owned by user', () => {
-        const otherUser = adminAuthRegister('otheruser@gmail.com', 'Password12', 'Joe', 'Mama');
-        const otherQuiz = adminQuizCreate(otherUser.authUserId, "Other User's Quiz", "Description");
-        expect(adminQuizInfo(user.authUserId, otherQuiz.quizId)).toStrictEqual(error);
+			const otherUser = adminAuthRegister('otheruser@gmail.com', 'Password12', 'Joe', 'Mama');
+			const otherQuiz = adminQuizCreate(otherUser.authUserId, "Other User's Quiz", "Description");
+			expect(adminQuizInfo(user.authUserId, otherQuiz.quizId)).toStrictEqual(error);
     });
-
     test('Quiz owned by user', () => {
-        expect(adminQuizInfo(user.authUserId, quizId)).not.toStrictEqual(error);
+			expect(adminQuizInfo(user.authUserId, quizId)).not.toStrictEqual(error);
     });
+	});
 });
 
