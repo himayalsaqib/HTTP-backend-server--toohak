8dbea7598bafe99fe6fa448e0324b18d5fd41b79
Haleemah Rahman <z5361900@ad.unsw.edu.au>
2024-06-20 21:00:45 +1000

    Fixed too deep indentation across whole file

--- src/quiz-tests/adminQuizRemove.test.js
+++ src/quiz-tests/adminQuizRemove.test.js
@@ -5,18 +5,16 @@ import {adminAuthRegister} from '../auth';
 import {clear} from '../other';
 
 beforeEach(()=> {
-    clear();
+  clear();
 });
-
 
 
 describe('adminQuizRemove', () => {
   let user;
   let quiz;
   beforeEach(() => {
-      user = adminAuthRegister('user@gmail.com', 'Password01', 'User', 'One').authUserId;
-      quiz = adminQuizCreate(user, 'Quiz 1', 'Description 1').quizId;
-      
+    user = adminAuthRegister('user@gmail.com', 'Password01', 'User', 'One').authUserId;
+    quiz = adminQuizCreate(user, 'Quiz 1', 'Description 1').quizId;
   });
 
   test('Has the correct return type', () => {
@@ -24,50 +22,43 @@ describe('adminQuizRemove', () => {
     expect(remove).toStrictEqual({});
   });
 
-    describe('Remove quiz from invalid ids returns error', () => {
-      test('Invalid authUserId, valid quizId', () => {
-          const invalidUserId = user + 1;
-          
-          expect(adminQuizRemove(invalidUserId, quiz)).toStrictEqual({ error: 'AuthUserId does not refer to a valid user id.' });
-      });
-
-      test('Valid authUserId, invalid quizId', () => {
-          const invalidQuizId = quiz + 1;
-
-          expect(adminQuizRemove(user, invalidQuizId)).
-          toStrictEqual({ error: 'Quiz Id does not refer to a valid quiz.' });
-      });
-    });
-    
-
-    describe('Returns error when quiz does not belong to user', () => {
-      test('Should not remove valid quiz that does not belong to user ', () => {
-          const user2 = adminAuthRegister('user2@gmail.com', 'Password02', 'User', 'Two').authUserId;            
-          expect(adminQuizRemove(user2, quiz)).toStrictEqual({ error: 'Quiz does not belong to user' });
-      }); 
+  describe('Remove quiz from invalid ids returns error', () => {
+    test('Invalid authUserId, valid quizId', () => {
+      const invalidUserId = user + 1;
+      expect(adminQuizRemove(invalidUserId, quiz)).toStrictEqual({ error: 'AuthUserId does not refer to a valid user id.' });
     });
 
-    
-    describe('Successful adminQuizRemove', () => {
+    test('Valid authUserId, invalid quizId', () => {
+      const invalidQuizId = quiz + 1;
+      expect(adminQuizRemove(user, invalidQuizId)).
+      toStrictEqual({ error: 'Quiz Id does not refer to a valid quiz.' });
+    });
+  });
+  
 
-        test('Successfully remove a quiz', () => {
-            expect(adminQuizRemove(user, quiz)).toStrictEqual({});
-        });
+  describe('Returns error when quiz does not belong to user', () => {
+    test('Should not remove valid quiz that does not belong to user ', () => {
+      const user2 = adminAuthRegister('user2@gmail.com', 'Password02', 'User', 'Two').authUserId;            
+      expect(adminQuizRemove(user2, quiz)).toStrictEqual({ error: 'Quiz does not belong to user' });
+    }); 
+  });
 
-        test('Remove a quiz that has already been successfully removed', () => {
-          adminQuizRemove(user, quiz);
-
-          expect(adminQuizRemove(user, quiz)).
-          toStrictEqual({ error: "Quiz Id does not refer to a valid quiz." });
-        });
-
-        test('Remove mulitple quizzes', () => {
-            const quiz2 = adminQuizCreate(user, 'Quiz 02', 'Description 02').quizId;
-
-            expect(adminQuizRemove(user, quiz)).toStrictEqual({});
-            expect(adminQuizRemove(user, quiz2)).toStrictEqual({});
-        });
-
+  
+  describe('Successful adminQuizRemove', () => {
+    test('Successfully remove a quiz', () => {
+      expect(adminQuizRemove(user, quiz)).toStrictEqual({});
     });
 
+    test('Remove a quiz that has already been successfully removed', () => {
+      adminQuizRemove(user, quiz);
+      expect(adminQuizRemove(user, quiz)).
+      toStrictEqual({ error: "Quiz Id does not refer to a valid quiz." });
+    });
+
+    test('Remove mulitple quizzes', () => {
+      const quiz2 = adminQuizCreate(user, 'Quiz 02', 'Description 02').quizId;
+      expect(adminQuizRemove(user, quiz)).toStrictEqual({});
+      expect(adminQuizRemove(user, quiz2)).toStrictEqual({});
+    });
+  });
 });
