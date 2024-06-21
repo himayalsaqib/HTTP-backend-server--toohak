e5c808686cb1959d337350bee5b89d4c3f76d727
Sameera Zaman <z5361907@ad.unsw.edu.au>
2024-06-21 14:37:05 +1000

    Removed helper functions from auth.js and imported them from helper.js

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,12 @@
 import { setData, getData } from './dataStore';
 import validator from 'validator';
+import { 
+  adminEmailInUse,
+  adminUserNameIsValid,
+  adminPasswordHasValidChars,
+  adminUserIdExists,
+  checkPasswordHistory,
+} from './helper'; 
 
 /////////////////////////////// Global Variables ///////////////////////////////
 const MIN_PASSWORD_LENGTH = 8;
@@ -258,97 +265,4 @@ export function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
   return {};
 }
 
-/////////////////////////////// Helper Functions ///////////////////////////////
 
-/**
- * Function checks if an email is already being used by an existing user
- *
- * @param {string} email
- * @returns {boolean} true if email exists, false if not
- */
-function adminEmailInUse(email) {
-  const data = getData();
-
-  const user = data.users.find(user => user.email === email);
-
-  if (user === undefined) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-/**
- * Function checks if a name is valid (ie doesn't contain a number or 
- * special characters other than spaces, hyphens or apostrophes)
- *
- * @param {string} name
- * @returns {boolean} true if a name is valid otherwise false
- */
-function adminUserNameIsValid(name) {
-  // specialCharacters will match any string that includes a special 
-  // character except for space, hyphen or apostrophe
-  const specialCharacters = /[^A-Za-z\s'-]/;
-  if (specialCharacters.test(name)) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-/**
- * Function checks whether the given password contains atleast one number and 
- * atleast one letter
- * 
- * @param {string} password to check
- * @returns {boolean} true if password has neccessary chars otherwise false
- */
-function adminPasswordHasValidChars(password) {
-  if (/\d/.test(password) && /[a-zA-Z]/.test(password)) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-/**
- * Function checks if an authUserId exists in the dataStore 
- *
- * @param {number} authUserId 
- * @returns {boolean} true if authUserId is valid otherwise false 
- */
-function adminUserIdExists(authUserId) {
-  const data = getData();
-
-  const user = data.users.find(user => user.authUserId === authUserId);
-
-  if (user === undefined) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-/**
- * Function checks previousPassword array to determine whether a user has already
- * used a password when updating the password
- * 
- * @param {number} authUserId
- * @param {number} newPassword
- * @returns {boolean} true if newPassword matches any previous passwords
- */
-function checkPasswordHistory(authUserId, newPassword) {
-  let data = getData();
-
-  for (const user of data.users) {
-    if (user.authUserId === authUserId) {
-      for (const password of user.previousPasswords) {
-        if (password === newPassword) {
-          return true;
-        } 
-      }
-    }
-  }
-
-  return false;
-}
