e6b818d5820eb03f99278fa994533a955ab32651
Tara Moore <z5487977@ad.unsw.edu.au>
2024-06-28 12:01:15 +1000

    fixed style issues with lint

--- src/other-tests/clear.test.ts
+++ src/other-tests/clear.test.ts
@@ -3,6 +3,7 @@ import { requestDelete, requestPost, requestGet } from '../requestHelper';
 describe('DELETE /v1/clear', () => {
   describe('Test for the return type of clear', () => {
     test('Has correct return type', () => {
+      // const res = requestDelete({}, '/v1/clear');
       const res = requestDelete('/v1/clear');
       expect(res.retval).toStrictEqual({});
       expect(res.statusCode).toStrictEqual(200);
@@ -10,9 +11,9 @@ describe('DELETE /v1/clear', () => {
   });
 
   describe('Testing side-effects of clear', () => {
-    let token: { authUserId: number };
+    let token: { sessionId: number };
     beforeEach(() => {
-      const res = requestPost({email: 'email@gmail.com', password: 'password123', nameFirst: 'Jane', nameLast: 'Doe'}, '/v1/admin/auth/register');
+      const res = requestPost({ email: 'email@gmail.com', password: 'password123', nameFirst: 'Jane', nameLast: 'Doe' }, '/v1/admin/auth/register');
       token = res.retval;
     });
 
@@ -21,11 +22,10 @@ describe('DELETE /v1/clear', () => {
     });
 
     test('Successful access of user details in adminUserDetails after logging in once', () => {
-      let newUserToken: { authUserId: number };
-      const resRegister = requestPost({email: 'valid@gmail.com', password: 'validpa55word', nameFirst: 'John', nameLast: 'Smith'}, '/v1/admin/auth/regiser');
-      newUserToken = resRegister.retval;
+      const resRegister = requestPost({ email: 'valid@gmail.com', password: 'validpa55word', nameFirst: 'John', nameLast: 'Smith' }, '/v1/admin/auth/regiser');
+      const newUserToken: { sessionId: number } = resRegister.retval;
 
-      expect(requestPost({ email: 'valid@gmail.com', password: 'validpa55word'}, '/v1/admin/auth/login')).toStrictEqual({ newUserToken: expect.any(Number) });
+      expect(requestPost({ email: 'valid@gmail.com', password: 'validpa55word' }, '/v1/admin/auth/login')).toStrictEqual({ newUserToken: expect.any(Number) });
 
       const resDetails = requestGet({ token: newUserToken }, '/v1/admin/user/details');
 
