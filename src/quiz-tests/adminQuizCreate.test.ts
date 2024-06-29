985578c8e28d87edd7bd9d3bd6a781831c767a8a
Sarah Townsley <z5481287@ad.unsw.edu.au>
2024-06-29 20:33:31 +1000

    separated tests by status code with describe functions

--- src/quiz-tests/adminQuizCreate.test.ts
+++ src/quiz-tests/adminQuizCreate.test.ts
@@ -32,55 +32,47 @@ describe('POST /v1/admin/quiz', () => {
     });
   });
 
-  describe('Testing user ID', () => {
-    test('Returns error when given invalid user ID', () => {
-      expect(adminQuizCreate(user.authUserId + 1, 'Valid Quiz Name',
-        'Valid quiz description.')).toStrictEqual(error);
+  describe('Testing status code 401', () => {
+    describe('Testing user ID', () => {
+      test('Returns error when given invalid user ID', () => {
+        expect(adminQuizCreate(user.authUserId + 1, 'Valid Quiz Name',
+          'Valid quiz description.')).toStrictEqual(error);
+      });
     });
-  });
+  })
 
-  describe('Testing quiz name errors', () => {
-    test('Quiz name contains invalid characters', () => {
-      expect(adminQuizCreate(user.authUserId, 'Invalid Name @#$%^&*',
-        'Valid quiz description.')).toStrictEqual(error);
+  describe('Testing status code 400', () => {
+    describe('Testing quiz name errors', () => {
+      test('Quiz name contains invalid characters', () => {
+        expect(adminQuizCreate(user.authUserId, 'Invalid Name @#$%^&*',
+          'Valid quiz description.')).toStrictEqual(error);
+      });
+  
+      test('Quiz name is less than 3 characters', () => {
+        expect(adminQuizCreate(user.authUserId, 'Hi',
+          'Valid quiz description.')).toStrictEqual(error);
+      });
+  
+      test('Quiz name is more than 30 characters', () => {
+        expect(adminQuizCreate(user.authUserId,
+          '1234567890 1234567890 1234567890',
+          'Valid quiz description.')).toStrictEqual(error);
+      });
+  
+      test('Quiz name already used by current user for another quiz', () => {
+        adminQuizCreate(user.authUserId, 'Name In Use', 'Pre existing quiz');
+  
+        expect(adminQuizCreate(user.authUserId, 'Name In Use',
+          'Valid quiz description.')).toStrictEqual(error);
+      });
     });
-
-    test('Quiz name is less than 3 characters', () => {
-      expect(adminQuizCreate(user.authUserId, 'Hi',
-        'Valid quiz description.')).toStrictEqual(error);
-    });
-
-    test('Quiz name is more than 30 characters', () => {
-      expect(adminQuizCreate(user.authUserId,
-        '1234567890 1234567890 1234567890',
-        'Valid quiz description.')).toStrictEqual(error);
-    });
-
-    test('Quiz name already used by current user for another quiz', () => {
-      adminQuizCreate(user.authUserId, 'Name In Use', 'Pre existing quiz');
-
-      expect(adminQuizCreate(user.authUserId, 'Name In Use',
-        'Valid quiz description.')).toStrictEqual(error);
-    });
-  });
-
-  describe('Testing description error', () => {
-    const longString = '1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890';
-
-    test('Quiz description is more than 100 characters', () => {
-      expect(adminQuizCreate(user.authUserId, 'Valid Quiz Name',
-        longString)).toStrictEqual(error);
-    });
-  });
-
-  describe('Testing side effect in adminQuizList', () => {
-    test('Check if adminQuizList has newly created quizzes', () => {
-      adminQuizCreate(user.authUserId, 'Quiz 1', '');
-      expect(adminQuizList(user.authUserId)).toStrictEqual({
-        quizzes: [{
-          quizId: expect.any(Number),
-          name: 'Quiz 1'
-        }]
+  
+    describe('Testing description error', () => {
+      const longString = '1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890';
+  
+      test('Quiz description is more than 100 characters', () => {
+        expect(adminQuizCreate(user.authUserId, 'Valid Quiz Name',
+          longString)).toStrictEqual(error);
       });
     });
   });
