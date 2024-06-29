22cbd17cd9763683304e318f5e45e57143453050
Sarah Townsley <z5481287@ad.unsw.edu.au>
2024-06-29 20:47:50 +1000

    modified tests for quiz name and quiz description to be HTTP under the title testing status code 400

--- src/quiz-tests/adminQuizCreate.test.ts
+++ src/quiz-tests/adminQuizCreate.test.ts
@@ -35,7 +35,7 @@ describe('POST /v1/admin/quiz', () => {
   describe('Testing status code 401', () => {
     describe('Testing invalid token', () => {
       test('Returns error when given invalid user ID', () => {
-        quizBody.sessionId += 1;
+        quizBody = { sessionId: token.sessionId + 1, name: 'Valid Quiz Name', description: 'Valid Quiz Description' };
         const res = requestPost(quizBody, '/v1/admin/quiz');
         expect(res.retval.toStrictEqual(error));
         expect(res.statusCode).toStrictEqual(401);
@@ -45,7 +45,7 @@ describe('POST /v1/admin/quiz', () => {
         token = { sessionId: null, authUserId: null };
         quizBody.sessionId = token.sessionId;
         const res = requestPost(quizBody, '/v1/admin/quiz');
-        
+
         expect(res.retval.toStrictEqual(error));
         expect(res.statusCode).toStrictEqual(401);
       });
@@ -55,26 +55,38 @@ describe('POST /v1/admin/quiz', () => {
   describe('Testing status code 400', () => {
     describe('Testing quiz name errors', () => {
       test('Quiz name contains invalid characters', () => {
-        expect(adminQuizCreate(user.authUserId, 'Invalid Name @#$%^&*',
-          'Valid quiz description.')).toStrictEqual(error);
+        quizBody = { sessionId: token.sessionId, name: 'Invalid Quiz Name !@#$%^&*()', description: 'Valid Quiz Description' };
+        const res = requestPost(quizBody, '/v1/admin/quiz');
+
+        expect(res.retval).toStrictEqual(error);
+        expect(res.statusCode).toStrictEqual(400);
       });
   
       test('Quiz name is less than 3 characters', () => {
-        expect(adminQuizCreate(user.authUserId, 'Hi',
-          'Valid quiz description.')).toStrictEqual(error);
+        quizBody = { sessionId: token.sessionId, name: 'Hi', description: 'Valid Quiz Description' };
+        const res = requestPost(quizBody, '/v1/admin/quiz');
+
+        expect(res.retval).toStrictEqual(error);
+        expect(res.statusCode).toStrictEqual(400);
       });
   
       test('Quiz name is more than 30 characters', () => {
-        expect(adminQuizCreate(user.authUserId,
-          '1234567890 1234567890 1234567890',
-          'Valid quiz description.')).toStrictEqual(error);
+        quizBody = { sessionId: token.sessionId, name: '1234567890 1234567890 1234567890', description: 'Valid Quiz Description' };
+        const res = requestPost(quizBody, '/v1/admin/quiz');
+
+        expect(res.retval).toStrictEqual(error);
+        expect(res.statusCode).toStrictEqual(400);
       });
   
       test('Quiz name already used by current user for another quiz', () => {
-        adminQuizCreate(user.authUserId, 'Name In Use', 'Pre existing quiz');
-  
-        expect(adminQuizCreate(user.authUserId, 'Name In Use',
-          'Valid quiz description.')).toStrictEqual(error);
+        quizBody = { sessionId: token.sessionId, name: 'Name In Use', description: 'Valid Quiz Description' };
+        requestPost(quizBody, '/v1/admin/quiz');
+
+        quizBody = { sessionId: token.sessionId, name: 'Name In Use', description: 'Different Quiz' };
+        const res = requestPost(quizBody, '/v1/admin/quiz');
+
+        expect(res.retval).toStrictEqual(error);
+        expect(res.statusCode).toStrictEqual(400);
       });
     });
   
@@ -82,8 +94,11 @@ describe('POST /v1/admin/quiz', () => {
       const longString = '1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890';
   
       test('Quiz description is more than 100 characters', () => {
-        expect(adminQuizCreate(user.authUserId, 'Valid Quiz Name',
-          longString)).toStrictEqual(error);
+        quizBody = { sessionId: token.sessionId, name: 'Valid Quiz Name', description: longString };
+        const res = requestPost(quizBody, '/v1/admin/quiz');
+
+        expect(res.retval).toStrictEqual(error);
+        expect(res.statusCode).toStrictEqual(400);
       });
     });
   });
