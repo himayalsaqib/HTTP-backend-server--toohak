a73c4c6d42ff98a09e1aa291b792f69b51fe4504
Himayal Saqib <z5534127@ad.unsw.edu.au>
2024-07-02 18:43:44 +1000

    fixed linting error in server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import sui from 'swagger-ui-express';
 import fs from 'fs';
 import path from 'path';
 import process from 'process';
-import { 
+import {
   adminAuthRegister,
   adminAuthLogin,
   adminUserDetails,
