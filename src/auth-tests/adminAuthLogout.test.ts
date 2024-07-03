61c4d9140dbe5b50d2be80cbf4ed7ed6b6f01b86
Himayal Saqib <z5534127@ad.unsw.edu.au>
2024-07-03 20:45:45 +1000

    wrote http tests for adminAuthLogout including return type, side effect and errors

---
+++ src/auth-tests/adminAuthLogout.test.ts
@@ -0,0 +1,72 @@
+// includes http tests for the route /v1/admin/auth/logout
+
+import { requestDelete, requestGet, requestPost } from "../helper-files/requestHelper";
+
+beforeEach(() => {
+	requestDelete({}, '/v1/clear');
+});
+
+describe('POST /v1/admin/auth/logout', () => {
+	const error = { error: expect.any(String) };
+	let bodyRegister: { email: string, password: string, nameFirst: string, nameLast: string };
+  let token: { sessionId: number, authUserId: number };
+
+	describe('Testing successful user logout (status code 200)', () => {
+		beforeEach(() => {
+      bodyRegister = { email: 'valid@gmail.com', password: 'Password12', nameFirst: 'Jane', nameLast: 'Doe' };
+      const { retval } = requestPost(bodyRegister, '/v1/admin/auth/register');
+      token = retval as { sessionId: number, authUserId: number };
+    });
+
+		test('Has the correct return type', () => {
+			expect(requestPost(token, '/v1/admin/auth/logout')).toStrictEqual({
+				retval: {},
+				statusCode: 200
+			});
+		});
+
+		test('Side effect: adminUserDetails returns error when called with a logged out token (sessionId is not valid)', () => {
+			expect(requestPost(token, '/v1/admin/auth/logout')).toStrictEqual({
+				retval: {},
+				statusCode: 200
+			});
+			expect(requestGet(token, '/v1/admin/user/details')).toStrictEqual({
+        retval: error,
+        statusCode: 401
+      });
+		})
+	});
+
+	describe('Testing token given to adminAuthLogout (status code 401)', () => {
+		test('Returns error when token is empty (no users are registered)', () => {
+      expect(requestPost(token, '/v1/admin/auth/logout')).toStrictEqual({
+        retval: error,
+        statusCode: 401
+      });
+    });
+
+    test('Returns error when authUserId is not a valid user', () => {
+      bodyRegister = { email: 'valid@gmail.com', password: 'Password12', nameFirst: 'Jane', nameLast: 'Doe' };
+      const { retval } = requestPost(bodyRegister, '/v1/admin/auth/register');
+      token = retval as { sessionId: number, authUserId: number };
+      token.authUserId += 1;
+
+      expect(requestPost(token, '/v1/admin/auth/logout')).toStrictEqual({
+        retval: error,
+        statusCode: 401
+      });
+    });
+
+    test('Returns error when sessionId is not a valid logged in user session', () => {
+      bodyRegister = { email: 'valid@gmail.com', password: 'Password12', nameFirst: 'Jane', nameLast: 'Doe' };
+      const { retval } = requestPost(bodyRegister, '/v1/admin/auth/register');
+      token = retval as { sessionId: number, authUserId: number };
+      token.sessionId += 1;
+
+      expect(requestPost(token, '/v1/admin/auth/logout')).toStrictEqual({
+        retval: error,
+        statusCode: 401
+      });
+    });
+	});
+});
