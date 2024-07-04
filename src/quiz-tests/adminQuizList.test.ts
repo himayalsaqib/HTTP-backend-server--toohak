dfec409e476b985bbc2d61a63e770d631f5901f2
Haleemah Rahman <z5361900@ad.unsw.edu.au>
2024-07-04 19:56:55 +1000

    Removed unnecessary comment

--- src/quiz-tests/adminQuizList.test.ts
+++ src/quiz-tests/adminQuizList.test.ts
@@ -1,7 +1,7 @@
 // includes http tests for the route /v1/admin/quiz/list
 
 import { Tokens } from '../dataStore';
-import { requestDelete, requestGet, requestPost } from '../helper-files/requestHelper'; // Adjust import based on your helper setup
+import { requestDelete, requestGet, requestPost } from '../helper-files/requestHelper'; 
 
 beforeEach(() => {
   requestDelete({}, '/v1/clear');
