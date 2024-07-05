e712a637966a52004315a4ea19fdd1865b6f5df8
Sarah Townsley <z5481287@ad.unsw.edu.au>
2024-07-06 01:04:55 +1000

    reordered tests to match swagger file

--- src/quiz-tests/adminQuizRestore.test.ts
+++ src/quiz-tests/adminQuizRestore.test.ts
@@ -49,90 +49,13 @@ describe('POST /v1/admin/quiz/:quizid/restore', () => {
     });
 
     test('Side effect (successful restoration): quizInfo displays correct timeLastEdited', () => {
-      const time = parseFloat((Date.now() / 1000).toFixed(10)) + 1;
+      const time = parseFloat((Date.now() / 1000).toFixed(10));
       let res = requestPost(token, `/v1/admin/quiz/${quizId}/restore`);
       expect(res).toStrictEqual({ retval: {}, statusCode: 200 });
 
       res = requestGet(token, `/v1/admin/quiz/${quizId}`);
-      expect(res.retval.timeLastEdited).toBeLessThanOrEqual(time);
-    });
-  });
-
-  describe('Testing quizId errors (status code 403)', () => {
-    beforeEach(() => {
-      // register a user
-      user = { email: 'valid1@gmail.com', password: 'Password12', nameFirst: 'Jane', nameLast: 'Doe' };
-      const { retval } = requestPost(user, '/v1/admin/auth/register');
-      token = retval as { sessionId: number, authUserId: number };
-
-      // create a quiz
-      quiz = { token: token, name: 'Valid Quiz Name', description: 'Valid Quiz Description' };
-      const createQuizRes = requestPost(quiz, '/v1/admin/quiz');
-      quizId = createQuizRes.retval.quizId;
-
-      // delete the quiz
-      requestDelete(token, `/v1/admin/quiz/${quizId}`);
-    });
-
-    test('Current user does not own the quiz', () => {
-      // register a second user
-      let user2: { email: string, password: string, nameFirst: string, nameLast: string };
-
-      user = { email: 'valid2@gmail.com', password: 'Password12', nameFirst: 'Jamie', nameLast: 'David' };
-      const { retval } = requestPost(user2, '/v1/admin/auth/register');
-      const token2 = retval as { sessionId: number, authUserId: number };
-
-      // create a quiz for the second user
-      quiz = { token: token2, name: 'Other Valid Quiz Name', description: 'Other Valid Quiz Description' };
-      const createQuizRes2 = requestPost(quiz, '/v1/admin/quiz');
-      const quizId2 = createQuizRes2.retval.quizId;
-
-      // delete second user's quiz
-      requestDelete(token2, `/v1/admin/quiz/${quizId2}`);
-
-      // first user tries to restore second user's deleted quiz
-      const res = requestPost(token, `/v1/admin/quiz/${quizId2}/restore`);
-      expect(res).toStrictEqual({ retval: error, statusCode: 403 });
-    });
-
-    test('Quiz ID does not exist', () => {
-      const res = requestPost(token, `/v1/admin/quiz/${quizId + 1}/restore`);
-      expect(res).toStrictEqual({ retval: error, statusCode: 403 });
-    });
-  });
-
-  describe('Testing token errors (status code 401)', () => {
-    beforeEach(() => {
-      // register a user
-      user = { email: 'valid1@gmail.com', password: 'Password12', nameFirst: 'Jane', nameLast: 'Doe' };
-      const { retval } = requestPost(user, '/v1/admin/auth/register');
-      token = retval as { sessionId: number, authUserId: number };
-
-      // create a quiz
-      quiz = { token: token, name: 'Valid Quiz Name', description: 'Valid Quiz Description' };
-      const createQuizRes = requestPost(quiz, '/v1/admin/quiz');
-      quizId = createQuizRes.retval.quizId;
-
-      // delete the quiz
-      requestDelete(token, `/v1/admin/quiz/${quizId}`);
-    });
-
-    test('Token is empty (no users registered)', () => {
-      requestDelete({}, '/v1/clear');
-      const res = requestPost(token, `/v1/admin/quiz/${quizId}/restore`);
-      expect(res).toStrictEqual({ retval: error, statusCode: 401 });
-    });
-
-    test('Session ID is invalid', () => {
-      token.sessionId += 1;
-      const res = requestPost(token, `/v1/admin/quiz/${quizId}/restore`);
-      expect(res).toStrictEqual({ retval: error, statusCode: 401 });
-    });
-
-    test('User ID is invalid', () => {
-      token.authUserId += 1;
-      const res = requestPost(token, `/v1/admin/quiz/${quizId}/restore`);
-      expect(res).toStrictEqual({ retval: error, statusCode: 401 });
+      expect(res.retval.timeLastEdited).toBeGreaterThanOrEqual(time);
+      expect(res.retval.timeLastEdited).toBeLessThanOrEqual(time + 1);
     });
   });
 
@@ -178,6 +101,84 @@ describe('POST /v1/admin/quiz/:quizid/restore', () => {
     test('Quiz ID refers to a quiz that is not currently in the trash', () => {
       const res = requestPost(token, `/v1/admin/quiz/${quizId}/restore`);
       expect(res).toStrictEqual({ retval: error, statusCode: 400 });
+    });
+  });
+
+  describe('Testing token errors (status code 401)', () => {
+    beforeEach(() => {
+      // register a user
+      user = { email: 'valid1@gmail.com', password: 'Password12', nameFirst: 'Jane', nameLast: 'Doe' };
+      const { retval } = requestPost(user, '/v1/admin/auth/register');
+      token = retval as { sessionId: number, authUserId: number };
+
+      // create a quiz
+      quiz = { token: token, name: 'Valid Quiz Name', description: 'Valid Quiz Description' };
+      const createQuizRes = requestPost(quiz, '/v1/admin/quiz');
+      quizId = createQuizRes.retval.quizId;
+
+      // delete the quiz
+      requestDelete(token, `/v1/admin/quiz/${quizId}`);
+    });
+
+    test('Token is empty (no users registered)', () => {
+      requestDelete({}, '/v1/clear');
+      const res = requestPost(token, `/v1/admin/quiz/${quizId}/restore`);
+      expect(res).toStrictEqual({ retval: error, statusCode: 401 });
+    });
+
+    test('Session ID is invalid', () => {
+      token.sessionId += 1;
+      const res = requestPost(token, `/v1/admin/quiz/${quizId}/restore`);
+      expect(res).toStrictEqual({ retval: error, statusCode: 401 });
+    });
+
+    test('User ID is invalid', () => {
+      token.authUserId += 1;
+      const res = requestPost(token, `/v1/admin/quiz/${quizId}/restore`);
+      expect(res).toStrictEqual({ retval: error, statusCode: 401 });
+    });
+  });
+
+  describe('Testing quizId errors (status code 403)', () => {
+    beforeEach(() => {
+      // register a user
+      user = { email: 'valid1@gmail.com', password: 'Password12', nameFirst: 'Jane', nameLast: 'Doe' };
+      const { retval } = requestPost(user, '/v1/admin/auth/register');
+      token = retval as { sessionId: number, authUserId: number };
+
+      // create a quiz
+      quiz = { token: token, name: 'Valid Quiz Name', description: 'Valid Quiz Description' };
+      const createQuizRes = requestPost(quiz, '/v1/admin/quiz');
+      quizId = createQuizRes.retval.quizId;
+
+      // delete the quiz
+      requestDelete(token, `/v1/admin/quiz/${quizId}`);
+    });
+
+    test('Current user does not own the quiz', () => {
+      // register a second user
+      let user2: { email: string, password: string, nameFirst: string, nameLast: string };
+
+      user = { email: 'valid2@gmail.com', password: 'Password12', nameFirst: 'Jamie', nameLast: 'David' };
+      const { retval } = requestPost(user2, '/v1/admin/auth/register');
+      const token2 = retval as { sessionId: number, authUserId: number };
+
+      // create a quiz for the second user
+      quiz = { token: token2, name: 'Other Valid Quiz Name', description: 'Other Valid Quiz Description' };
+      const createQuizRes2 = requestPost(quiz, '/v1/admin/quiz');
+      const quizId2 = createQuizRes2.retval.quizId;
+
+      // delete second user's quiz
+      requestDelete(token2, `/v1/admin/quiz/${quizId2}`);
+
+      // first user tries to restore second user's deleted quiz
+      const res = requestPost(token, `/v1/admin/quiz/${quizId2}/restore`);
+      expect(res).toStrictEqual({ retval: error, statusCode: 403 });
+    });
+
+    test('Quiz ID does not exist', () => {
+      const res = requestPost(token, `/v1/admin/quiz/${quizId + 1}/restore`);
+      expect(res).toStrictEqual({ retval: error, statusCode: 403 });
     });
   });
 });
