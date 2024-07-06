942cb5d9e4f2108c3453a4ea452abb9981a551e5
Haleemah Rahman <z5361900@ad.unsw.edu.au>
2024-07-06 11:12:15 +1000

    Added http testing for adminQuizTrash, UNFINISHED

---
+++ src/quiz-tests/adminQuizTrash.test.ts
@@ -0,0 +1,98 @@
+// tests for adminQuizTrashEmpty function
+
+import { Tokens } from '../dataStore';
+import { requestDelete, requestGet, requestPost } from '../helper-files/requestHelper';
+
+beforeEach(() => {
+  requestDelete({}, '/v1/clear');
+});
+
+describe('GET /v1/admin/quiz/trash', () => {
+  let userBody: { email: string, password: string, nameFirst: string, nameLast: string };
+  let quizBody: { token: Tokens, name: string, description: string };
+  let token: { sessionId: number, authUserId: number };
+  const error = { error: expect.any(String) };
+
+  describe('Testing for correct return type (status code 200)', () => {
+    beforeEach(() => {
+      userBody = { email: 'userone@gmail.com', password: 'Password01', nameFirst: 'User', nameLast: 'One' };
+      const { retval } = requestPost(userBody, '/v1/admin/auth/register');
+      token = retval as { sessionId: number, authUserId: number };
+      quizBody = { token: token, name: 'My Quiz Name', description: 'Valid Quiz Description' };
+    });
+
+    test('Correctly displays a quiz in trash', () => {
+      const res = requestPost(quizBody, '/v1/admin/quiz');
+      const quizId = res.retval.quizId;
+      requestDelete(token, `/v1/admin/quiz/${quizId}`);
+
+      const trashRes = requestGet(token, '/v1/admin/quiz/trash');
+      expect(trashRes.statusCode).toBe(200);
+      expect(trashRes.retval.quizzes).toStrictEqual({
+        retval: {
+          quizzes: [
+            {
+              quizId: res.retval.quizId,
+              name: 'My Quiz Name'
+            }
+          ]
+        },
+        statusCode: 200
+      });
+    });
+
+    test('Correctly displays multiple quizzes in trash', () => {
+      const res = requestPost(quizBody, '/v1/admin/quiz');
+      const quizId = res.retval.quizId;
+      requestDelete(token, `/v1/admin/quiz/${quizId}`);
+
+      quizBody = { token: token, name: 'My Quiz Two', description: 'Other Quiz Description' };
+      const res2 = requestPost(quizBody, '/v1/admin/quiz');
+      const quizId2 = res2.retval.quizId;
+      requestDelete(token, `/v1/admin/quiz/${quizId2}`);
+
+      const trashRes = requestGet(token, '/v1/admin/quiz/trash');
+      expect(trashRes.retval.quizzes).toStrictEqual({
+        retval: {
+          quizzes: [
+            {
+              quizId: res.retval.quizId,
+              name: 'My Quiz Name'
+            },
+            {
+              quizId: res2.retval.quizId,
+              name: 'My Quiz Name'
+            }
+          ]
+        },
+        statusCode: 200
+      });
+    });
+
+    test.skip('Side effect: correctly displays trash after quiz had been restored', () => {
+      let res = requestPost(quizBody, '/v1/admin/quiz');
+      const quizId = res.retval.quizId;
+      requestDelete(token, `/v1/admin/quiz/${quizId}`);
+
+      res = requestPost(token, `/v1/admin/quiz/${quizId}/restore`);
+      const trashRes = requestGet(token, '/v1/admin/quiz/trash');
+      expect(trashRes).toStrictEqual({ retval: { quizzes: [] }, statusCode: 200 });
+    });
+  });
+
+  test.skip('Side effect: correctly displays empty trash after trash has been emptied', () => {
+    let res = requestPost(quizBody, '/v1/admin/quiz');
+    const quizId = res.retval.quizId;
+    requestDelete(token, `/v1/admin/quiz/${quizId}`);
+
+    res = requestDelete(token, '/v1/admin/quiz/trash/empty');
+    const trashRes = requestGet(token, '/v1/admin/quiz/trash');
+    expect(trashRes).toStrictEqual({ retval: { quizzes: [] }, statusCode: 200 });
+  });
+});
+
+describe('Testing for empty or invalid Token (status code 401)', () => {
+  test('', () => {
+
+  });
+});
