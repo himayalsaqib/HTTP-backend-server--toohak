d77e5c19379b9ad76724d29235362698868634db
Himayal Saqib <z5534127@ad.unsw.edu.au>
2024-07-09 13:00:42 +1000

    wrote the implementation for adminQuizQuestionUpdate but some functions used are on the quizQuestionCreate branch so show errors

--- src/quiz.ts
+++ src/quiz.ts
@@ -24,6 +24,11 @@ export interface QuizInfo extends Omit<Quizzes, 'authUserId'> {
   duration: number,
 }
 
+interface QuestionAnswer {
+  answer: string, 
+  correct: boolean
+}
+
 /// ////////////////////////////// Functions ///////////////////////////////////
 /**
  * Provide a list of all quizzes that are owned by the currently logged in user.
@@ -251,6 +256,100 @@ export function adminQuizDescriptionUpdate (authUserId: number, quizId: number, 
   quiz.timeLastEdited = Math.floor(Date.now() / 1000);
 
   const data = getData();
+  setData(data);
+
+  return {};
+}
+
+/**
+ * Update the description of the relevant quiz
+ *
+ * @param {number} authUserId
+ * @param {number} quizId
+ * @param {number} questionId
+ * @param {string} questionName
+ * @param {number} duration
+ * @param {number} points 
+ * @param {QuestionAnswer[]} answers
+ * @returns {{} | { error: string }} 
+ */
+export function adminQuizQuestionUpdate(
+  authUserId: number,
+  quizId: number,
+  questionId: number,
+  questionBody: QuestionBody
+): EmptyObject | ErrorObject {
+  if (questionBody.question.length < MIN_QUESTION_LEN || questionBody.question.length > MAX_QUESTION_LEN) {
+    return { error: 'Question  is less than 5 characters or greater than 50 characters.' };
+  }
+  if (questionBody.answers.length < MIN_NUM_ANSWERS || questionBody.answers.length > MAX_NUM_ANSWERS) {
+    return { error: 'Question has more than 6 answers or less than 2 answers.'};
+  }
+  if (questionBody.duration <= 0) {
+    return { error: 'Question duration is not a positive number.'};
+  }
+  // sum of quiz questions duration exceeds 3 minutes. function
+  if (calculateSumQuestionDuration(quizId, questionBody.duration) > MAX_QUIZ_QUESTIONS_DURATION) {
+    return { error: 'Sum of the question durations in the quiz exceeds 3 minutes.' };
+  }
+  if (questionBody.points < MIN_POINT_VALUE || questionBody.points > MAX_POINT_VALUE) {
+    return { error: 'Points awarded for the question are less than 1 or greater than 10.' };
+  }
+  // The length of any answer is shorter than 1 character long, or longer than 30 characters long. function
+  if (checkAnswerLength(questionBody, MIN_ANS_LEN, MAX_ANS_LEN) === true) {
+    return { error: 'length of any answer is shorter than 1 character, or longer than 30 characters.' };
+  }
+  // Any answer strings are duplicates of one another (within the same question). function
+  if (checkForAnsDuplicates(questionBody) === true) {
+    return { error: 'Any answer strings are duplicates of one another.' };
+  }
+  // There are no correct answers. function
+  if (checkForNumCorrectAns(questionBody) < MIN_CORRECT_ANS) {
+    return { error: 'There are no correct answers.' };
+  }
+  if (authUserIdExists(authUserId) === false) {
+    return { error: 'AuthUserId is not a valid user.' };
+  }
+  if (quizIdInUse(quizId) === false) {
+    return { error: 'Quiz ID does not refer to a valid quiz.' };
+  }
+
+  const data = getData();
+  const quiz = findQuizById(quizId);
+
+  if (quiz.authUserId !== authUserId) {
+    return { error: 'Quiz ID does not refer to a quiz that this user owns.' };
+  }
+  
+  quiz.timeLastEdited = Math.floor(Date.now() / 1000);
+
+  if (questionIdInUse(questionId, quizId) === false) {
+    return { error: 'Question Id does not refer to a valid question within this quiz.'};
+  }
+
+  let questionToUpdate = findQuestionById(questionId, quizId);
+
+  questionToUpdate.question = questionBody.question;
+  questionToUpdate.duration = questionBody.duration;
+  questionToUpdate.points = questionBody.points;
+  questionToUpdate.answers = [];
+  const answerColours = ['red', 'blue', 'green', 'yellow', 'purple', 'brown', 'orange'];
+  for (const index in questionBody.answers) {
+    let newAnswerId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+    while (answerIdInUse(newAnswerId, questionId, quizId) === true) {
+      newAnswerId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+    }
+ 
+    questionToUpdate.answers.push({
+      answerId: newAnswerId,
+      answer: questionBody.answers[index].answer,
+      colour: answerColours[index], //assign a colour based on its index
+      correct: questionBody.answers[index].correct
+    });
+  }
+
+  //update quiz duration.
+
   setData(data);
 
   return {};
