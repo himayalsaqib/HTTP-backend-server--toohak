c00bcc6c5ac39dfffbb7a41f6251941e5bb62dd9
Sarah Townsley <z5481287@ad.unsw.edu.au>
2024-07-09 20:45:07 +1000

    server route for /v1/admin/quiz/list now takes in token as a string of sessionID instead of as an object and uses sessionIdExists and findTokenFromSessionId to find the token object

--- src/server.ts
+++ src/server.ts
@@ -205,16 +205,20 @@ app.delete('/v1/admin/quiz/:quizid', (req: Request, res: Response) => {
 });
 
 app.get('/v1/admin/quiz/list', (req: Request, res: Response) => {
-  const sessionId = parseInt(req.query.sessionId as string);
-  const authUserId = parseInt(req.query.authUserId as string);
-  const token = { sessionId: sessionId, authUserId: authUserId };
+  const sessionId = parseInt(req.query.token as string);
 
-  let response = tokenExists(token);
+  if (sessionIdExists(sessionId) === false) {
+    return(res).status(401).json({ error: 'Invalid session ID' });
+  }
+
+  let userToken = findTokenFromSessionId(sessionId);
+
+  let response = tokenExists(userToken);
   if ('error' in response) {
     return res.status(401).json(response);
   }
 
-  response = adminQuizList(token.authUserId);
+  response = adminQuizList(userToken.authUserId);
   res.json(response);
 });
 
