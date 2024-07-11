8f6a9ac422c6cff62d0be41f569d607de926caa6
Sameera Zaman <z5361907@ad.unsw.edu.au>
2024-07-11 15:22:46 +1000

    Added other test cases to adminQuizQuestionDelete.test.ts

--- src/quiz-tests/adminQuizQuestionDelete.test.ts
+++ src/quiz-tests/adminQuizQuestionDelete.test.ts
@@ -13,6 +13,8 @@ describe('DELETE /v1/admin/quiz/{quizid}/question/{questionid}', () => {
     let token: string;
     let answerBody: { answer: string, correct: boolean }[];
     let questionBody: { question: string, duration: number, points: number, answers: { answer: string, correct: boolean }[]  };
+	let quizId: number;
+	let questionId: number;
 
 beforeEach(() => {
 	userBody = { email: 'valid@gmail.com', password: 'Password12', nameFirst: 'Jane', nameLast: 'Doe' };
@@ -38,12 +40,50 @@ beforeEach(() => {
 	};
 
 	const questionResponse = requestPost(questionBody, `/v1/admin/quiz/${quizId}/question`);
-  const questionId = questionResponse.retval.questionId;
+  	const questionId = questionResponse.retval.questionId;
 
 	});
 	test('Successfully deletes a question', () => {
-		//const res = requestDelete({ token: token }, `/v1/admin/quiz/${quizId}/question/${questionId}`);
-
+		const res = requestDelete({ token: token }, `/v1/admin/quiz/${quizId}/question/${questionId}`);
+		expect(res).toStrictEqual({ retval: {}, statusCode: 200 });
 	});
 
+	describe('Testing parameters given to adminQuizQuestionUpdate (status code 400)', () => {
+		describe('Testing questionId errors', () => {
+		  test('Question Id does not refer to a valid question within this quiz', () => {
+			questionId++;
+			const res = requestDelete({ token: token }, `/v1/admin/quiz/${quizId}/question/${questionId}`);
+			expect(res).toStrictEqual({ retval: error, statusCode: 400 });
+		  });
+		});
+	
+});
+	describe('Testing token errors (status code 401)', () => {
+		test('Returns error when token is empty', () => {
+			requestDelete({}, '/v1/clear');
+			const res = requestDelete({ token: token }, `/v1/admin/quiz/${quizId}/question/${questionId}`);
+			expect(res).toStrictEqual({ retval: error, statusCode: 401 });
+		});
+
+		test('Returns error when sessionId is not a valid user session', () => {
+			const sessionId = parseInt(token) + 1;
+			const res = requestDelete({ token: token }, `/v1/admin/quiz/${quizId}/question/${questionId}`);
+			expect(res).toStrictEqual({ retval: error, statusCode: 401
+			});
+		});
+	});
+
+	describe('Testing quizId errors (status code 403)', () => {
+    test('Returns error when user is not an owner of the quiz', () => {
+      const user2 = { email: 'valid1@gmail.com', password: 'Password12', nameFirst: 'John', nameLast: 'Doe' };
+      //let res = requestPost(user2, '/v1/admin/auth/register');
+      //updateBody.token = res.retval.token;
+
+      const res = requestDelete({ token: token }, `/v1/admin/quiz/${quizId}/question/${questionId}`);
+      expect(res).toStrictEqual({ retval: error, statusCode: 403 });
+    });
+
+
+
+});
 });
