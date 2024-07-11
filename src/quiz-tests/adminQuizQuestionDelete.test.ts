d1708a5ca541a9544997d9c6d0d34afeb862807a
Sameera Zaman <z5361907@ad.unsw.edu.au>
2024-07-11 15:55:11 +1000

    Completed HTTP tests for adminQuizQuestionDelete.test.ts

--- src/quiz-tests/adminQuizQuestionDelete.test.ts
+++ src/quiz-tests/adminQuizQuestionDelete.test.ts
@@ -13,8 +13,9 @@ describe('DELETE /v1/admin/quiz/{quizid}/question/{questionid}', () => {
     let token: string;
     let answerBody: { answer: string, correct: boolean }[];
     let questionBody: { question: string, duration: number, points: number, answers: { answer: string, correct: boolean }[]  };
-	let quizId: number;
-	let questionId: number;
+		let quizId: number;
+		let questionId: number;
+		let questionId2: number;
 
 beforeEach(() => {
 	userBody = { email: 'valid@gmail.com', password: 'Password12', nameFirst: 'Jane', nameLast: 'Doe' };
@@ -33,19 +34,74 @@ beforeEach(() => {
 	];
 
 	questionBody = {
-		question: 'Sample Question?',
+		question: 'Sample Question 1?',
 		duration: 30,
 		points: 10,
 		answers: answerBody
 	};
 
 	const questionResponse = requestPost(questionBody, `/v1/admin/quiz/${quizId}/question`);
-  	const questionId = questionResponse.retval.questionId;
+  questionId = questionResponse.retval.questionId;
 
-	});
+	questionBody.question = 'Sample Question 2?';
+  const questionResponse2 = requestPost(questionBody, `/v1/admin/quiz/${quizId}/question`);
+  questionId2 = questionResponse2.retval.questionId;
+
+});
+
 	test('Successfully deletes a question', () => {
 		const res = requestDelete({ token: token }, `/v1/admin/quiz/${quizId}/question/${questionId}`);
 		expect(res).toStrictEqual({ retval: {}, statusCode: 200 });
+	});
+
+	test('Side effect: adminQuizInfo returns quiz without the deleted question', () => {
+		requestDelete({ token: token }, `/v1/admin/quiz/${quizId}/question/${questionId}`);
+
+		const res = requestGet({ token }, `/v1/admin/quiz/${quizId}`);
+		expect(res.retval).toStrictEqual({
+			quizId: quizId,
+			name: quizBody.name,
+			timeCreated: expect.any(Number),
+			timeLastEdited: expect.any(Number),
+			description: quizBody.description,
+			numQuestions: 1,
+			questions: [
+				{
+					questionId: questionId2,
+					question: 'Sample Question 2?',
+					duration: 30,
+					points: 10,
+					answers: [
+						{
+							answerId: expect.any(Number),
+							answer: 'Answer 1',
+							colour: expect.any(String),
+							correct: false
+						},
+						{
+							answerId: expect.any(Number),
+							answer: 'Answer 2',
+							colour: expect.any(String),
+							correct: true
+						},
+						{
+							answerId: expect.any(Number),
+							answer: 'Answer 3',
+							colour: expect.any(String),
+							correct: false
+						},
+						{
+							answerId: expect.any(Number),
+							answer: 'Answer 4',
+							colour: expect.any(String),
+							correct: false
+						}
+					]
+				}
+			],
+			duration: 30
+		});
+		expect(res.statusCode).toStrictEqual(200);
 	});
 
 	describe('Testing parameters given to adminQuizQuestionUpdate (status code 400)', () => {
@@ -76,14 +132,18 @@ beforeEach(() => {
 	describe('Testing quizId errors (status code 403)', () => {
     test('Returns error when user is not an owner of the quiz', () => {
       const user2 = { email: 'valid1@gmail.com', password: 'Password12', nameFirst: 'John', nameLast: 'Doe' };
-      //let res = requestPost(user2, '/v1/admin/auth/register');
-      //updateBody.token = res.retval.token;
+      const user2Res = requestPost(user2, '/v1/admin/auth/register');
+      const otherUserToken: string = user2Res.retval.token;
 
-      const res = requestDelete({ token: token }, `/v1/admin/quiz/${quizId}/question/${questionId}`);
+			// user 2 tries to delete user 1's quiz question. 
+			const res = requestDelete({ token: otherUserToken }, `/v1/admin/quiz/${quizId}/question/${questionId}`);
       expect(res).toStrictEqual({ retval: error, statusCode: 403 });
     });
 
-
-
-});
+		test('Returns error when quiz doesn\'t exist', () => {
+      quizId++;
+      const res = requestDelete({ token: token }, `/v1/admin/quiz/${quizId}/question/${questionId}`);
+      expect(res).toStrictEqual({ retval: error, statusCode: 403 });
+		});
+	});
 });
