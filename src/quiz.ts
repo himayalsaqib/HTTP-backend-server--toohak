988111861d5cf0173adcee5968d7225f61663009
Sameera Zaman <z5361907@ad.unsw.edu.au>
2024-07-11 22:49:43 +1000

    Added comments to adminQuizTrashEmpty

--- src/quiz.ts
+++ src/quiz.ts
@@ -322,9 +322,12 @@ export function adminQuizTrashEmpty(authUserId: number, quizIds: number[]): Empt
     }
   }
 
-  const quizzesNotInTrash = quizIds.filter(quizId => !(data.trash.some(q => q.quiz.quizId === quizId)));
-  if (quizzesNotInTrash.length > 0) {
-    return { error: 'One or more Quiz IDs is not currently in the trash.' };
+  // Find the first quizId in quizIds that is not in data.trash for every quizId 
+  const quizNotInTrash = quizIds.find(quizId => data.trash.every(q => q.quiz.quizId !== quizId));
+
+  // If not undefined, there is at least one quizId not in data.trash 
+  if (quizNotInTrash !== undefined) {
+  return { error: 'One or more Quiz IDs is not currently in the trash.' };
   }
 
   for (const quizId of quizIds) {
