c42b179abb59e6e939d08ca098497d43f8e01d8a
Sarah Townsley <z5481287@ad.unsw.edu.au>
2024-07-16 15:30:55 +1000

    added optional headers parameter to request helper functions

--- src/helper-files/requestHelper.ts
+++ src/helper-files/requestHelper.ts
@@ -2,52 +2,57 @@
 
 import request from 'sync-request-curl';
 import { port, url } from '../config.json';
+import IncomingHttpHeaders from 'http';
 
 const SERVER_URL = `${url}:${port}`;
 const TIMEOUT_MS = 5 * 1000;
 
-export function requestGet(qs: object, path: string) {
+export function requestGet(qs: object, path: string, header?: IncomingHttpHeaders.IncomingHttpHeaders) {
   const res = request(
     'GET',
     SERVER_URL + path,
     {
       qs: qs,
-      timeout: TIMEOUT_MS,
+      headers: header,
+      timeout: TIMEOUT_MS
     }
   );
   return { retval: JSON.parse(res.body.toString()), statusCode: res.statusCode };
 }
 
-export function requestDelete(qs: object, path: string) {
+export function requestDelete(qs: object, path: string, header?: IncomingHttpHeaders.IncomingHttpHeaders) {
   const res = request(
     'DELETE',
     SERVER_URL + path,
     {
       qs: qs,
+      headers: header,
       timeout: TIMEOUT_MS
     }
   );
   return { retval: JSON.parse(res.body.toString()), statusCode: res.statusCode };
 }
 
-export function requestPut(body: object, path: string) {
+export function requestPut(body: object, path: string, header?: IncomingHttpHeaders.IncomingHttpHeaders) {
   const res = request(
     'PUT',
     SERVER_URL + path,
     {
       json: body,
+      headers: header,
       timeout: TIMEOUT_MS
     }
   );
   return { retval: JSON.parse(res.body.toString()), statusCode: res.statusCode };
 }
 
-export function requestPost(body: object, path: string) {
+export function requestPost(body: object, path: string, header?: IncomingHttpHeaders.IncomingHttpHeaders) {
   const res = request(
     'POST',
     SERVER_URL + path,
     {
       json: body,
+      headers: header,
       timeout: TIMEOUT_MS
     }
   );
