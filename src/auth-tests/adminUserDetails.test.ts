e1e93ea1d16bc457c3a54c2b05f5a09d7a5c2c04
Himayal Saqib <z5534127@ad.unsw.edu.au>
2024-07-16 20:11:00 +1000

    created another describe for GET /v2/admin/user/details tests where token is passed in header

--- src/auth-tests/adminUserDetails.test.ts
+++ src/auth-tests/adminUserDetails.test.ts
@@ -103,3 +103,101 @@ describe('GET /v1/admin/user/details', () => {
     });
   });
 });
+
+describe('GET /v2/admin/user/details', () => {
+  const error = { error: expect.any(String) };
+  let body: { email: string, password: string, nameFirst: string, nameLast: string };
+  let token: string;
+
+  beforeEach(() => {
+    body = { email: 'valid@gmail.com', password: 'Password12', nameFirst: 'Jane', nameLast: 'Doe' };
+    const registerResponse = requestPost(body, '/v1/admin/auth/register');
+    token = registerResponse.retval.token;
+  });
+
+  describe('Testing for correct return type (status code 200)', () => {
+    test('Successful return of user details when only one user is registered', () => {
+      expect(requestGet({}, '/v2/admin/user/details', { token })).toStrictEqual({
+        retval: {
+          user: {
+            userId: expect.any(Number),
+            name: `${body.nameFirst} ${body.nameLast}`,
+            email: body.email,
+            numSuccessfulLogins: 1,
+            numFailedPasswordsSinceLastLogin: 0,
+          }
+        },
+        statusCode: 200
+      });
+    });
+
+    test('Successful return of user details when multiple users are registered', () => {
+      // second registration
+      const body2 = { email: 'valid2@gmail.com', password: 'Password12', nameFirst: 'John', nameLast: 'Doe' };
+      requestPost(body2, '/v1/admin/auth/register');
+
+      expect(requestGet({}, '/v2/admin/user/details', { token })).toStrictEqual({
+        retval: {
+          user: {
+            userId: expect.any(Number),
+            name: `${body.nameFirst} ${body.nameLast}`,
+            email: body.email,
+            numSuccessfulLogins: 1,
+            numFailedPasswordsSinceLastLogin: 0,
+          }
+        },
+        statusCode: 200
+      });
+    });
+
+    test('Can view multiple user details correctly', () => {
+      // second registration
+      const body2 = { email: 'valid2@gmail.com', password: 'Password12', nameFirst: 'John', nameLast: 'Doe' };
+      const registerResponse2 = requestPost(body2, '/v1/admin/auth/register');
+      const token2 = registerResponse2.retval.token;
+
+      expect(requestGet({}, '/v2/admin/user/details', { token })).toStrictEqual({
+        retval: {
+          user: {
+            userId: expect.any(Number),
+            name: `${body.nameFirst} ${body.nameLast}`,
+            email: body.email,
+            numSuccessfulLogins: 1,
+            numFailedPasswordsSinceLastLogin: 0,
+          }
+        },
+        statusCode: 200
+      });
+      expect(requestGet({}, '/v2/admin/user/details', { token: token2 })).toStrictEqual({
+        retval: {
+          user: {
+            userId: expect.any(Number),
+            name: `${body2.nameFirst} ${body2.nameLast}`,
+            email: body2.email,
+            numSuccessfulLogins: 1,
+            numFailedPasswordsSinceLastLogin: 0,
+          }
+        },
+        statusCode: 200
+      });
+    });
+  });
+
+  describe('Testing authUserId given to adminUserDetails (status code 401)', () => {
+    test('Returns error when token is empty (no users are registered)', () => {
+      requestDelete({}, '/v1/clear');
+      expect(requestGet({}, '/v2/admin/user/details', { token })).toStrictEqual({
+        retval: error,
+        statusCode: 401
+      });
+    });
+
+    test('Returns error when sessionId is not a valid logged in user session', () => {
+      const sessionId = parseInt(token) + 1;
+      expect(requestGet({}, '/v2/admin/user/details', { token: sessionId.toString() })).toStrictEqual({
+        retval: error,
+        statusCode: 401
+      });
+    });
+  });
+});
