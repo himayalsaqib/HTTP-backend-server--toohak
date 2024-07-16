592bc9c94739c170a7e90c2261b0235b8b723966
Himayal Saqib <z5534127@ad.unsw.edu.au>
2024-07-16 15:07:40 +1000

    added another describe to test file for /v2/admin/auth/logout and edited request helpers to pass token as header.

--- src/auth-tests/adminAuthLogout.test.ts
+++ src/auth-tests/adminAuthLogout.test.ts
@@ -1,4 +1,4 @@
-// includes http tests for the route /v1/admin/auth/logout
+// includes http tests for the route /v1/admin/auth/logout and /v2/admin/auth/logout
 
 import { requestDelete, requestGet, requestPost } from '../helper-files/requestHelper';
 
@@ -60,6 +60,67 @@ describe('POST /v1/admin/auth/logout', () => {
     test('Returns error when sessionId is not a valid logged in user session', () => {
       const sessionId = parseInt(token) + 1;
       expect(requestPost({ token: sessionId.toString() }, '/v1/admin/auth/logout')).toStrictEqual({
+        retval: error,
+        statusCode: 401
+      });
+    });
+  });
+});
+
+describe('POST /v2/admin/auth/logout', () => {
+  const error = { error: expect.any(String) };
+  let bodyRegister: { email: string, password: string, nameFirst: string, nameLast: string };
+  let token: string;
+
+  beforeEach(() => {
+    bodyRegister = { email: 'valid@gmail.com', password: 'Password12', nameFirst: 'Jane', nameLast: 'Doe' };
+    const registerResponse = requestPost(bodyRegister, '/v2/admin/auth/register');
+    token = registerResponse.retval.token;
+  });
+
+  describe('Testing successful user logout (status code 200)', () => {
+    test('Has the correct return type', () => {
+      expect(requestPost({}, { token }, '/v2/admin/auth/logout')).toStrictEqual({
+        retval: {},
+        statusCode: 200
+      });
+    });
+
+    test('Side effect: adminUserDetails returns error when called with a logged out token (invalid)', () => {
+      expect(requestPost({}, { token }, '/v2/admin/auth/logout')).toStrictEqual({
+        retval: {},
+        statusCode: 200
+      });
+      expect(requestGet({}, { token }, '/v2/admin/user/details')).toStrictEqual({
+        retval: error,
+        statusCode: 401
+      });
+    });
+
+    test('Side effect: adminAuthLogout returns error when user tries to logout same token twice', () => {
+      expect(requestPost({}, { token }, '/v2/admin/auth/logout')).toStrictEqual({
+        retval: {},
+        statusCode: 200
+      });
+      expect(requestPost({}, { token }, '/v2/admin/auth/logout')).toStrictEqual({
+        retval: error,
+        statusCode: 401
+      });
+    });
+  });
+
+  describe('Testing token given to adminAuthLogout (status code 401)', () => {
+    test('Returns error when token is empty', () => {
+      requestDelete({}, '/v1/clear');
+      expect(requestPost({}, { token }, '/v2/admin/auth/logout')).toStrictEqual({
+        retval: error,
+        statusCode: 401
+      });
+    });
+
+    test('Returns error when sessionId is not a valid logged in user session', () => {
+      const sessionId = parseInt(token) + 1;
+      expect(requestPost({}, { token: sessionId.toString() }, '/v2/admin/auth/logout')).toStrictEqual({
         retval: error,
         statusCode: 401
       });
