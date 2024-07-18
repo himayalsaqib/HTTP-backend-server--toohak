ad6b52679ba307e353171e45d3f69b036829c83c
Haleemah Rahman <z5361900@ad.unsw.edu.au>
2024-07-18 18:02:28 +1000

    Skipped test using quiz remove as it hasn't been implemented yet

--- src/quiz-tests/adminQuizList.test.ts
+++ src/quiz-tests/adminQuizList.test.ts
@@ -1,4 +1,4 @@
-// includes http tests for the route /v1/admin/quiz/list
+// includes http tests for the route /v1/admin/quiz/list and /v2/admin/quiz/list
 
 import { requestDelete, requestGet, requestPost } from '../helper-files/requestHelper';
 
@@ -6,11 +6,12 @@ beforeEach(() => {
   requestDelete({}, '/v1/clear');
 });
 
+const ERROR = { error: expect.any(String) };
+
 describe('GET /v1/admin/quiz/list', () => {
   let userBody: { email: string, password: string, nameFirst: string, nameLast: string };
   let quizBody: { token: string, name: string, description: string };
   let token: string;
-  const error = { error: expect.any(String) };
 
   describe('Testing for correct return type (status code 200)', () => {
     beforeEach(() => {
@@ -95,7 +96,7 @@ describe('GET /v1/admin/quiz/list', () => {
     test('Returns error when token is empty', () => {
       requestDelete({}, '/v1/clear');
       expect(requestGet({ token: token }, '/v1/admin/quiz/list')).toStrictEqual({
-        retval: error,
+        retval: ERROR,
         statusCode: 401
       });
     });
@@ -103,7 +104,110 @@ describe('GET /v1/admin/quiz/list', () => {
     test('Returns error when sessionId is invalid', () => {
       const sessionId = parseInt(token) + 1;
       expect(requestGet({ token: sessionId.toString }, '/v1/admin/quiz/list')).toStrictEqual({
-        retval: error,
+        retval: ERROR,
+        statusCode: 401
+      });
+    });
+  });
+});
+
+describe('GET /v2/admin/quiz/list', () => {
+  let userBody: { email: string, password: string, nameFirst: string, nameLast: string };
+  let quizBody: { token: string, name: string, description: string };
+  let token: string;
+
+  describe('Testing for correct return type (status code 200)', () => {
+    beforeEach(() => {
+      userBody = { email: 'user@gmail.com', password: 'Password01', nameFirst: 'User', nameLast: 'One' };
+      const registerUser = requestPost(userBody, '/v1/admin/auth/register');
+      token = registerUser.retval.token;
+      quizBody = { token: token, name: 'My Quiz Name', description: 'Valid Quiz Description' };
+    });
+
+    test('Correctly returns quiz list that contains 1 quiz', () => {
+      const res = requestPost(quizBody, '/v1/admin/quiz');
+      const listRes = requestGet({}, '/v2/admin/quiz/list', { token });
+      expect(listRes).toStrictEqual({
+        retval: {
+          quizzes: [
+            {
+              quizId: res.retval.quizId,
+              name: 'My Quiz Name',
+            }
+          ]
+        },
+        statusCode: 200
+      });
+    });
+
+    test('Correctly returns quiz list that contains multiple quizzes', () => {
+      const res = requestPost(quizBody, '/v1/admin/quiz');
+      quizBody = { token: token, name: 'My Quiz Two', description: 'Other Quiz Description' };
+      const res2 = requestPost(quizBody, '/v1/admin/quiz');
+      const listRes = requestGet({}, '/v2/admin/quiz/list', { token });
+      expect(listRes).toStrictEqual({
+        retval: {
+          quizzes: [
+            {
+              quizId: res.retval.quizId,
+              name: 'My Quiz Name',
+            },
+            {
+              quizId: res2.retval.quizId,
+              name: 'My Quiz Two',
+            }
+          ]
+        },
+        statusCode: 200
+      });
+    });
+
+    test.skip('Correctly returns quiz list after a quiz has been removed', () => {
+      const res = requestPost(quizBody, '/v1/admin/quiz');
+      quizBody = { token: token, name: 'My Quiz Two', description: 'Other Quiz Description' };
+      const res2 = requestPost(quizBody, '/v1/admin/quiz');
+      const quizId2 = res2.retval.quizId;
+
+      requestDelete({ token: token }, `/v1/admin/quiz/${quizId2}`);
+      const listRes = requestGet({}, '/v2/admin/quiz/list', { token });
+
+      expect(listRes).toStrictEqual({
+        retval: {
+          quizzes: [
+            {
+              quizId: res.retval.quizId,
+              name: 'My Quiz Name',
+            }
+          ]
+        },
+        statusCode: 200
+      });
+    });
+
+    test('Correctly returns quiz list that contains no quizzes', () => {
+      const listRes = requestGet({}, '/v2/admin/quiz/list', { token });
+      expect(listRes).toStrictEqual({
+        retval: {
+          quizzes: []
+        },
+        statusCode: 200
+      });
+    });
+  });
+
+  describe('Testing invalid token (status code 401)', () => {
+    test('Returns error when token is empty', () => {
+      requestDelete({}, '/v1/clear');
+      expect(requestGet({}, '/v2/admin/quiz/list', { token })).toStrictEqual({
+        retval: ERROR,
+        statusCode: 401
+      });
+    });
+
+    test('Returns error when sessionId is invalid', () => {
+      const sessionId = (parseInt(token) + 1).toString();
+      expect(requestGet({}, '/v2/admin/quiz/list', { token: sessionId })).toStrictEqual({
+        retval: ERROR,
         statusCode: 401
       });
     });
