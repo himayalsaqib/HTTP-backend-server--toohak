43c530ca35743bc7bf0705aaed7f1e4719886f0d
Sameera Zaman <z5361907@ad.unsw.edu.au>
2024-07-24 07:15:18 +1000

    push newPlayer into session

--- src/player.ts
+++ src/player.ts
@@ -13,30 +13,29 @@ import { QuizSessionState } from './quiz';
  * @returns {{ playerId: number}}
  */
 export function playerJoin(sessionId: number, name: string): { playerId: number } {
+  const session = findQuizSessionById(sessionId);
+  if (!session) {
+    throw new Error('Session Id does not refer to a valid session');
+  }
+
   let playerName = name;
 
   if (playerName === '') {
     playerName = generateRandomName();
-    while (playerNameExists(playerName)) {
+    while (playerNameExists(sessionId, playerName)) {
       playerName = generateRandomName();
     }
   }
 
   const data = getData();
 
-  if (playerNameExists(playerName)) {
+  if (playerNameExists(sessionId, playerName)) {
     throw new Error('Name is not unique');
   }
-  const session = findQuizSessionById(sessionId);
-  if (!session) {
-    throw new Error('Session Id does not refer to a valid session');
-  }
-
   // throw error for if session is not in lobby state
   if (session.state !== QuizSessionState.LOBBY) {
     throw new Error('Session is not in LOBBY state');
   }
-
   let newPlayerId = getRandomInt();
   while (playerIdInUse(newPlayerId)) {
     newPlayerId = getRandomInt();
@@ -47,6 +46,7 @@ export function playerJoin(sessionId: number, name: string): { playerId: number 
     name: playerName,
   };
 
+  session.players.push(newPlayer);
   data.players.push(newPlayer);
 
   setData(data);
