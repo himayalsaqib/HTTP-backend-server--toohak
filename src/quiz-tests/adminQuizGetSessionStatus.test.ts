0a46d8bb611c4670be01732235ebaca2f0ce1af2
Tara Moore <z5487977@ad.unsw.edu.au>
2024-07-23 21:01:48 +1000

    added describe blocks for all status codes tested and add all tests to do

--- src/quiz-tests/adminQuizGetSessionStatus.test.ts
+++ src/quiz-tests/adminQuizGetSessionStatus.test.ts
@@ -4,4 +4,28 @@ import { requestGet, requestDelete, requestPost, requestPut } from "../helper-fi
 
 beforeEach(() => {
   requestDelete({}, `/v1/clear`);
-})
+});
+
+describe('GET /v1/admin/quiz/{quiz}/session/{sessionid}', () => {
+  describe('Testing successful cases (status code 200)', () => {
+    test.todo('Has the correct return type');
+
+    test.todo('Side-effect tests');
+  });
+
+  describe('Testing session ID errors (status code 400)', () => {
+    test.todo('The session ID does not refer to a valid session within the quiz');
+  });
+
+  describe('Testing token errors (status code 401)', () => {
+    test.todo('The token is empty (no users are registered)');
+
+    test.todo('The token is invalid (does not refer to a valid logged in user)');
+  });
+
+  describe('Testing quiz ownership and quiz existence errors (status code 403)', () => {
+    test.todo('The user is not a owner of the quiz');
+
+    test.todo('This quiz does not exist');
+  });
+});
