a5ce705bcd3a9e5d72da48ade8f42fa72aabe770
Sameera Zaman <z5361907@ad.unsw.edu.au>
2024-07-23 12:14:13 +1000

    Added tests for 400 errors in playerJoin.test.ts

--- src/player-tests/playerJoin.test.ts
+++ src/player-tests/playerJoin.test.ts
@@ -1,6 +1,7 @@
 // contains HTTP tests for route POST /v1/player/join
 
 import { requestPost } from '../helper-files/requestHelper';
+import { QuestionBody, QuizSessionState } from '../quiz';
 
 const ERROR = { error: expect.any(String) };
 
@@ -9,12 +10,53 @@ beforeEach(() => {
 });
 
 describe('POST /v1/player/join', () => {
-  let userBody: { name: string };
+  let userBody: { email: string, password: string, nameFirst: string, nameLast: string };
+  let token: string;
+  let quizBody: { name: string, description: string };
+  let quizId: number;
+  let createBody: { questionBody: QuestionBody };
+  let questionId: number;
+  let startSessionBody: { autoStartNum: number };
+  let sessionId: number;
+  let playerBody: { sessionId: number, name: string };
+
+  beforeEach(() => {
+    // registering a user
+    userBody = { email: 'valid@gmail.com', password: 'Password12', nameFirst: 'Jane', nameLast: 'Doe' };
+    const registerResponse = requestPost(userBody, '/v1/admin/auth/register');
+    token = registerResponse.retval.token;
+
+    // creating a quiz
+    quizBody = { name: 'Quiz Name', description: 'Quiz description' };
+    const quizResponse = requestPost(quizBody, '/v2/admin/quiz', { token });
+    quizId = quizResponse.retval.quizId;
+
+    // creating a quiz question
+    createBody = {
+      questionBody: {
+        question: 'Who is the Monarch of England?',
+        duration: 5,
+        points: 5,
+        answers: [
+          { answer: 'Prince Charles', correct: true },
+          { answer: 'Prince William', correct: false }
+        ],
+        thumbnailUrl: 'http://google.com/some/image/path.png'
+      }
+    };
+    const createResponse = requestPost(createBody, `/v2/admin/quiz/${quizId}/question`, { token });
+    questionId = createResponse.retval.questionId;
+
+    // initialising body for start session route
+    startSessionBody = { autoStartNum: 3 };
+    const sessionResponse = requestPost(startSessionBody, `/v1/admin/quiz/${quizId}/session/start`, { token });
+    sessionId = sessionResponse.retval.sessionId;
+  });
   
   describe('Testing for correct return type (status code 200)', () => {
     test('Join with non-empty name', () => {
-      userBody = { name: 'JohnDoe' };
-      const res = requestPost(userBody, '/v1/player/join');
+      playerBody = { sessionId: sessionId, name: 'JohnDoe' };
+      const res = requestPost(playerBody, '/v1/player/join');
       expect(res).toStrictEqual({
         retval: {
           playerId: expect.any(Number),
@@ -25,8 +67,8 @@ describe('POST /v1/player/join', () => {
     });
 
     test('Join with empty name', () => {
-      userBody = { name: '' };
-      const res = requestPost(userBody, '/v1/player/join');
+      playerBody = { sessionId: sessionId, name: '' };
+      const res = requestPost(playerBody, '/v1/player/join');
       expect(res).toStrictEqual({
         retval: {
           playerId: expect.any(Number),
@@ -36,4 +78,27 @@ describe('POST /v1/player/join', () => {
       });
     });
   });
+
+  describe('Testing for error cases (status code 400)', () => {
+    test('Join with non-unique name', () => {
+      playerBody = { sessionId: sessionId, name: 'JohnDoe' };
+      requestPost(playerBody, '/v1/player/join');
+      const res = requestPost(playerBody, '/v1/player/join'); // Attempt to join with the same name again
+      expect(res).toStrictEqual({ retval: ERROR, statusCode: 400 });
+    });
+  });
+
+  test('Join with invalid session ID', () => {
+    playerBody = { sessionId: sessionId + 1, name: 'JaneDoe' };
+    const res = requestPost( playerBody, '/v1/player/join');
+    expect(res).toStrictEqual({ retval: ERROR, statusCode: 400 });
+  });
+
+  test.skip('Join when session is not in LOBBY state', () => {
+    // make state: QuizSessionState.END,
+    playerBody = { sessionId: sessionId, name: 'AliceDoe' };
+    requestPost({}, `/v1/admin/quiz/${quizId}/session/${sessionId}/start`, { token });
+    const joinResponse = requestPost({ ...playerBody, sessionId }, '/v1/player/join');
+  });
+    
 });
