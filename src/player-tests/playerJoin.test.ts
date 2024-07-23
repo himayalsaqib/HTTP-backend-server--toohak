ebc522843a795692ec352ed7e69f4b9755a10906
Sameera Zaman <z5361907@ad.unsw.edu.au>
2024-07-24 00:28:14 +1000

    Condensed side-effects test

--- src/player-tests/playerJoin.test.ts
+++ src/player-tests/playerJoin.test.ts
@@ -1,7 +1,7 @@
 // contains HTTP tests for route POST /v1/player/join
 
 import { requestDelete, requestPost, requestPut, requestGet } from '../helper-files/requestHelper';
-import { QuestionBody, QuizSessionState } from '../quiz';
+import { QuestionBody } from '../quiz';
 
 const ERROR = { error: expect.any(String) };
 
@@ -15,7 +15,6 @@ describe('POST /v1/player/join', () => {
   let quizBody: { name: string, description: string };
   let quizId: number;
   let createBody: { questionBody: QuestionBody };
-  let questionId: number;
   let startSessionBody: { autoStartNum: number };
   let sessionId: number;
   let playerBody: { sessionId: number, name: string };
@@ -46,8 +45,7 @@ describe('POST /v1/player/join', () => {
         thumbnailUrl: 'http://google.com/some/image/path.png'
       }
     };
-    const createResponse = requestPost(createBody, `/v2/admin/quiz/${quizId}/question`, { token });
-    questionId = createResponse.retval.questionId;
+    requestPost(createBody, `/v2/admin/quiz/${quizId}/question`, { token });
 
     // initialising body for start session route
     startSessionBody = { autoStartNum: 3 };
@@ -86,37 +84,10 @@ describe('POST /v1/player/join', () => {
       requestPost(playerBody, '/v1/player/join');
 
       const res = requestGet({}, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
-      expect(res.retval).toStrictEqual({
-        state: QuizSessionState.LOBBY,
-        atQuestion: 1,
-        players: [
-          'JaneDoe',
-          expect.stringMatching(/^[a-zA-Z]{5}\d{3}$/),
-        ],
-        metadata: {
-          quizId: quizId,
-          name: quizBody.name,
-          timeCreated: expect.any(Number),
-          timeLastEdited: expect.any(Number),
-          description: quizBody.description,
-          numQuestions: 1,
-          questions: [
-            {
-              questionId: questionId,
-              question: createBody.questionBody.question,
-              duration: createBody.questionBody.duration,
-              thumbnailUrl: createBody.questionBody.thumbnailUrl,
-              points: createBody.questionBody.points,
-              answers: [
-                { answerId: expect.any(Number), answer: 'Prince Charles', colour: expect.any(String), correct: true },
-                { answerId: expect.any(Number), answer: 'Prince William', colour: expect.any(String), correct: false }
-              ]
-            }
-          ],
-          duration: createBody.questionBody.duration,
-          thumbnailUrl: createBody.questionBody.thumbnailUrl
-        }
-      });
+      expect(res.retval.players).toStrictEqual([
+        'JaneDoe',
+        expect.stringMatching(/^[a-zA-Z]{5}\d{3}$/),
+      ]);
     });
   });
 
