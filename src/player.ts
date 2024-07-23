c7b248b10f36787038e5218cebbe399a73ded7c6
Sameera Zaman <z5361907@ad.unsw.edu.au>
2024-07-23 20:56:24 +1000

    Remove debug comments

--- src/player.ts
+++ src/player.ts
@@ -3,6 +3,16 @@
 import { setData, getData, ErrorObject, EmptyObject, Question, Answer, QuizSessions, Player } from './dataStore';
 import { generateRandomName, playerIdInUse, findQuizSessionById } from './helper-files/helper';
 
+export enum QuizSessionState {
+  LOBBY = 'LOBBY',
+  QUESTION_COUNTDOWN = 'QUESTION_COUNTDOWN',
+  QUESTION_OPEN = 'QUESTION_OPEN',
+  QUESTION_CLOSE = 'QUESTION_CLOSE',
+  ANSWER_SHOW = 'ANSWER_SHOW',
+  FINAL_RESULTS = 'FINAL_RESULTS',
+  END = 'END'
+}
+
 // =============================== FUNCTIONS ================================ //
 /**
  * Allow a guest player to join a session
@@ -19,37 +29,37 @@ export function playerJoin (sessionId: number, name: string): {playerId: number}
     }
 
   const data = getData();
-  const nameExists = data.players.find(q => q.name === name);
-  /*
-  for (const player of data.quizzes) {
-    if (player.name === name) {
-      return true;
-    }
-  }
-  */
-  if (nameExists) {
-    throw new Error('Name is not unique');
-  }
+  const nameExists = data.players.find(q => q.name === playerName);
 
-  if (findQuizSessionById === undefined) {
+
+  if (nameExists) {
+   throw new Error('Name is not unique');
+  }
+  const session = data.quizSessions.find((session) => session.sessionId === sessionId);
+  if (!session) {
     throw new Error('Session Id does not refer to a valid session')
   }
 
-  // if session is not in lobby state 
+  // throw error for if session is not in lobby state 
+  if (session.state !== QuizSessionState.LOBBY) {
+    throw new Error('Session is not in LOBBY state');
+  }
 
   let newPlayerId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
   while (playerIdInUse(newPlayerId) === true) { 
-  newPlayerId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+    newPlayerId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
   }
 
   const newPlayer = {
     playerId: newPlayerId,
-    name: name, 
+    name: playerName, 
   };
 
   data.players.push(newPlayer);
+
   setData(data);
 
   return { playerId: newPlayerId };
 
 }
+
