38aafdb29b8d7b30dc43870aa21d4af4e581506f
Tara Moore <z5487977@ad.unsw.edu.au>
2024-07-24 19:33:12 +1000

    wrote correct return test and one side-effect test

--- src/quiz-tests/adminQuizGetSessionStatus.test.ts
+++ src/quiz-tests/adminQuizGetSessionStatus.test.ts
@@ -1,16 +1,163 @@
 // includes http tests for the route /v1/admin/quiz/{quizid}/session/{sessionid}
 
+import { questionIdInUse } from "../helper-files/helper";
 import { requestGet, requestDelete, requestPost, requestPut } from "../helper-files/requestHelper";
+import { QuestionBody } from "../quiz";
 
 beforeEach(() => {
   requestDelete({}, `/v1/clear`);
 });
 
-describe('GET /v1/admin/quiz/{quiz}/session/{sessionid}', () => {
-  describe('Testing successful cases (status code 200)', () => {
-    test.todo('Has the correct return type');
+const ERROR = { error: expect.any(String) };
 
-    test.todo('Side-effect tests');
+describe('GET /v1/admin/quiz/{quiz}/session/{sessionid}', () => {
+  let userBody: { email: string, password: string, nameFirst: string, nameLast: string };
+  let token: string;
+  let quizBody: { name: string, description: string };
+  let quizId: number;
+  let questionBody: QuestionBody;
+  let sessionStartBody: { autoStartNum: number };
+  let sessionId: number;
+  let updateActionBody: { action: string };
+  let playerId: number;
+
+  beforeEach(() => {
+    // register user
+    // register user
+    userBody = { email: 'valid@gmail.com', password: 'Password123', nameFirst: 'Jane', nameLast: 'Doe' };
+    const registerRes = requestPost(userBody, '/v1/admin/auth/register');
+    token = registerRes.retval.token;
+
+    // create a quiz
+    quizBody = { name: 'Quiz Name', description: 'Valid quiz description' };
+    const quizRes = requestPost(quizBody, '/v2/admin/quiz', { token });
+    quizId = quizRes.retval.quizId;
+
+    // question a question
+    questionBody = {
+      question: 'Who is your favourite artist\'s favourite artist?',
+      duration: 7,
+      points: 9,
+      answers: [
+        { answer: 'Chappell Roan', correct: true },
+        { answer: 'Sabrina Carpenter', correct: false }
+      ],
+      thumbnailUrl: 'http://google.com/some/file/path.png'
+    };
+
+    requestPost({ questionBody }, `/v2/admin/quiz/${quizId}/question`, { token });
+
+    // start a session
+    sessionStartBody = { autoStartNum: 3 };
+    const sessionStartRes = requestPost(sessionStartBody, `/v1/admin/quiz/${quizId}/session/start`, { token });
+    sessionId = sessionStartRes.retval.sessionId;
+
+    // initalising updateActionBody for route
+    updateActionBody = { action: 'NEXT_QUESTION' };
+
+    // have a player join the session
+    const res = requestPost({ sessionId: sessionId, name: 'Jane' }, '/v1/player/join');
+    playerId = res.retval.playerId;
+  });
+  
+  describe('Testing successful cases (status code 200)', () => {
+    test('Has the correct return type', () => {
+      const getStatusRes = requestGet({}, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
+      expect(getStatusRes).toStrictEqual({
+        retval: {
+          state: 'LOBBY',
+          atQuestion: 0,
+          players: [
+            'Jane'
+          ],
+          metadata: {
+            quizId: quizId,
+            name: quizBody.name,
+            timeCreated: expect.any(Number),
+            timeLastEdited: expect.any(Number),
+            description: quizBody.description,
+            numQuestions: 1,
+            questions: [
+              {
+                questionId: expect.any(Number),
+                question: questionBody.question,
+                duration: questionBody.duration,
+                thumbnailUrl: questionBody.thumbnailUrl,
+                points: expect.any(Number),
+                answers: [
+                  {
+                    answerId: expect.any(Number),
+                    answer: questionBody.answers[0].answer,
+                    colour: expect.any(String),
+                    correct: questionBody.answers[0].correct
+                  }, {
+                    answerId: expect.any(Number),
+                    answer: questionBody.answers[1].answer,
+                    colour: expect.any(String),
+                    correct: questionBody.answers[1].correct
+                  }
+                ]
+              }
+            ],
+            duration: questionBody.duration,
+            thumbnail: expect.any(String),
+          }
+        }, 
+        statusCode: 200
+      });
+    });
+
+    test('Side-effect test: the correct status is shown when an action command is sent', () => {
+      const stateUpdateRes = requestPut(updateActionBody, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
+      expect(stateUpdateRes).toStrictEqual({
+        retval: {}, 
+        statusCode: 200
+      });
+
+      const getSessionRes = requestGet({}, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
+      expect(getSessionRes).toStrictEqual({
+        retval: {
+          state: 'QUESTION_COUNTDOWN',
+          atQuestion: 0,
+          players: [
+            'Jane'
+          ],
+          metadata: {
+            quizId: quizId,
+            name: quizBody.name,
+            timeCreated: expect.any(Number),
+            timeLastEdited: expect.any(Number),
+            description: quizBody.description,
+            numQuestions: 1,
+            questions: [
+              {
+                questionId: expect.any(Number),
+                question: questionBody.question,
+                duration: questionBody.duration,
+                thumbnailUrl: questionBody.thumbnailUrl,
+                points: expect.any(Number),
+                answers: [
+                  {
+                    answerId: expect.any(Number),
+                    answer: questionBody.answers[0].answer,
+                    colour: expect.any(String),
+                    correct: questionBody.answers[0].correct
+                  }, {
+                    answerId: expect.any(Number),
+                    answer: questionBody.answers[1].answer,
+                    colour: expect.any(String),
+                    correct: questionBody.answers[1].correct
+                  }
+                ]
+              }
+            ],
+            duration: questionBody.duration,
+            thumbnail: expect.any(String),
+          }
+        }, 
+        statusCode: 200,
+      });
+    });
   });
 
   describe('Testing session ID errors (status code 400)', () => {
