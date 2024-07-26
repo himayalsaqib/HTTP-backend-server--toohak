3747c85d0faf9640b2312bf00b658042e625f4ce
Tara Moore <z5487977@ad.unsw.edu.au>
2024-07-26 17:31:46 +1000

    wrote tests for all error cases and for the correct return type

--- src/quiz-tests/adminQuizSessionFinalResults.test.ts
+++ src/quiz-tests/adminQuizSessionFinalResults.test.ts
@@ -1,7 +1,7 @@
 // includes http tests for the route /v1/admin/quiz/{quizid}/session/{sessionid}/results
 
 import { requestGet, requestDelete, requestPost, requestPut } from "../helper-files/requestHelper";
-import { QuestionBody } from "../quiz";
+import { QuestionBody, QuizSessionAction } from "../quiz";
 
 const ERROR = expect.any(String);
 
@@ -15,6 +15,7 @@ describe('GET /v1/admin/quiz/{quizid}/session/{sessionid}/results', () => {
   let quizBody: { name: string, description: string };
   let quizId: number;
   let questionBody: QuestionBody;
+  let questionId: number;
   let sessionStartBody: { autoStartNum: number };
   let sessionId: number;
   let updateActionBody: { action: string };
@@ -42,8 +43,9 @@ describe('GET /v1/admin/quiz/{quizid}/session/{sessionid}/results', () => {
       thumbnailUrl: 'http://google.com/some/file/path.png'
     };
 
-    requestPost({ questionBody }, `/v2/admin/quiz/${quizId}/question`, { token });
-    
+    const questionRes = requestPost({ questionBody }, `/v2/admin/quiz/${quizId}/question`, { token });
+    questionId = questionRes.retval.questionId;
+
     // start a session i.e. state = LOBBY
     sessionStartBody = { autoStartNum: 4 };
     const sessionStartRes = requestPost(sessionStartBody, `/v1/admin/quiz/${quizId}/session/start`, { token });
@@ -51,11 +53,39 @@ describe('GET /v1/admin/quiz/{quizid}/session/{sessionid}/results', () => {
 
     // have a player join the session
     requestPost({ sessionId: sessionId, name: 'Jane' }, '/v1/player/join');
+
+    // update state to FINAL_RESULTS
+    requestPut({ action: QuizSessionAction.NEXT_QUESTION }, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
+    requestPut({ action: QuizSessionAction.SKIP_COUNTDOWN }, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
+    
+    // player submits answer
+
+    requestPut({ action: QuizSessionAction.GO_TO_ANSWER }, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
+    requestPut({ action: QuizSessionAction.GO_TO_FINAL_RESULTS }, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
   })
   
-  describe('Testing successful cases (status code 200)', () => {
+  describe.skip('Testing successful cases (status code 200)', () => {
     test('Has the correct return type with one player', () => {
-      // 
+      const sessionFinalRes = requestGet({}, `/v1/admin/quiz/${quizId}/session/${sessionId}/results`, { token });
+      expect(sessionFinalRes).toStrictEqual({
+        retval: {
+          usersRankedByScore: [
+            {
+              name: 'Jane',
+              score: expect.any(Number),
+            }
+          ],
+          questionResults: [
+            {
+              questionId: questionId,
+              playersCorrectList: [],
+              averageAnswerTime: expect.any(Number),
+              percentCorrect: expect.any(Number),
+            }
+          ]
+        },
+        statusCode: 200
+      }); 
     });
 
     test('Side-effect: Correctly lists usersRankedByScore in descending order with multiple players', () => {
@@ -64,20 +94,65 @@ describe('GET /v1/admin/quiz/{quizid}/session/{sessionid}/results', () => {
   });
 
   describe('Testing errors in sessionId and quiz session state (status code 400)', () => {
-    test.todo('The session Id does not refer to a valid session within this quiz');
+    test('The session Id does not refer to a valid session within this quiz', () => {
+      const res = requestGet({}, `/v1/admin/quiz/${quizId}/session/${sessionId + 1}/results`, { token });
+      expect(res).toStrictEqual({
+        retval: ERROR,
+        statusCode: 400
+      });
+    });
 
-    test.todo('The session is not in FINAL_RESULTS state');
+    test('The session is not in FINAL_RESULTS state', () => {
+      // make session go to END state
+      requestPut({ action: QuizSessionAction.END }, `/v1/admin/quiz/${quizId}/session${sessionId}`, { token });
+
+      const res = requestGet({}, `/v1/admin/quiz/${quizId}/session/${sessionId}/results`, { token });
+      expect(res).toStrictEqual({
+        retval: ERROR,
+        statusCode: 400
+      });
+    });
   });
 
   describe('Testing token errors (status code 401)', () => {
-    test.todo('The token is empty (no users are registered)');
+    test('The token is empty (no users are registered)', () => {
+      requestDelete({}, '/v1/clear');
+      const res = requestGet({}, `/v1/admin/quiz/${quizId}/session/${sessionId}/results`, { token });
+      expect(res).toStrictEqual({
+        retval: ERROR,
+        statusCode: 401
+      });
+    });
 
-    test.todo('The token is invalid (does not refer to a valid logged in user)');
+    test('The token is invalid (does not refer to a valid logged in user)', () => {
+      const invalidToken = parseInt(token) + 1;
+      const res = requestGet({}, `/v1/admin/quiz/${quizId}/session/${sessionId}/results`, { token: invalidToken.toString() });
+      expect(res).toStrictEqual({
+        retval: ERROR,
+        statusCode: 401
+      });
+    });
   });
 
   describe('Testing quiz ownership and quiz existence errors (status code 403)', () => {
-    test.todo('The user is not an owner of this quiz');
+    test('The user is not an owner of this quiz', () => {
+      const userBody2 = { email: 'email@gmail.com', password: 'validpa55w0rd', nameFirst: 'Betty', nameLast: 'Smith' };
+      const user2Res = requestPost(userBody2, '/v1/admin/auth/register');
+      token = user2Res.retval.token;
 
-    test.todo('The quiz does not exist');
+      const res = requestGet({}, `/v1/admin/quiz/${quizId}/session/${sessionId}/results`, { token });
+      expect(res).toStrictEqual({
+        retval: ERROR,
+        statusCode: 403
+      });
+    });
+
+    test('The quiz does not exist', () => {
+      const res = requestGet({}, `/v1/admin/quiz/${quizId + 1}/session/${sessionId}/results`, { token });
+      expect(res).toStrictEqual({
+        retval: ERROR,
+        statusCode: 403
+      });
+    });
   });
 });
