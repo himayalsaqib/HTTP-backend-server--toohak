64305ef03291f9cc55b9159a88b374ba079ef950
Sameera Zaman <z5361907@ad.unsw.edu.au>
2024-07-27 20:43:14 +1000

    Completed tests error cases 400

--- src/player-tests/playerQuestionInformation.test.ts
+++ src/player-tests/playerQuestionInformation.test.ts
@@ -1,5 +1,6 @@
 // contains HTTP tests for route GET /v1/player/{playerid}/question/{questionposition}
 
+import sleepSync from 'slync';
 import { requestDelete, requestPost, requestPut, requestGet } from '../helper-files/requestHelper';
 import { QuestionBody, QuizSessionAction } from '../quiz';
 
@@ -23,6 +24,7 @@ describe('POST /v1/player/join', () => {
   let playerId: number;
   let questionposition: number;
   let questionId: number; 
+  let questionId2: number; 
 
   beforeEach(() => {
     // registering a user
@@ -35,7 +37,7 @@ describe('POST /v1/player/join', () => {
     const quizResponse = requestPost(quizBody, '/v2/admin/quiz', { token });
     quizId = quizResponse.retval.quizId;
 
-    // creating a quiz question
+    // creating quiz question 1
     createBody = {
       questionBody: {
         question: 'Who is the Monarch of England?',
@@ -48,8 +50,25 @@ describe('POST /v1/player/join', () => {
         thumbnailUrl: 'http://google.com/some/image/path.png'
       }
     };
-    const qeustionResponse = requestPost(createBody, `/v2/admin/quiz/${quizId}/question`, { token });
-    questionId = qeustionResponse.retval.questionId;
+    const questionResponse = requestPost(createBody, `/v2/admin/quiz/${quizId}/question`, { token });
+    questionId = questionResponse.retval.questionId;
+
+     // creating quiz question 2
+     createBody = {
+       questionBody: {
+         question: 'Which is the second largest city in Japan?',
+         duration: 5,
+         points: 5,
+         answers: [
+          { answer: 'Tokyo', correct: false },
+          { answer: 'Osaka', correct: true },
+          { answer: 'Kyoto', correct: false }
+         ],
+         thumbnailUrl: 'http://example.com/image2.png'
+       }
+     };
+     const questionResponse2 = requestPost(createBody, `/v2/admin/quiz/${quizId}/question`, { token });
+     questionId2 = questionResponse2.retval.questionId;
 
     // initialising body for start session route
     startSessionBody = { autoStartNum: 2 };
@@ -59,17 +78,17 @@ describe('POST /v1/player/join', () => {
     // player joins session
     playerBody = { sessionId: sessionId, name: 'JaneDoe' };
     const res = requestPost(playerBody, '/v1/player/join');
-
-    // initialising updateActionBody for route
-    updateActionBody = { action: QuizSessionAction.NEXT_QUESTION };
-    requestPut(updateActionBody, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
-    updateActionBody = { action: QuizSessionAction.SKIP_COUNTDOWN };
-    requestPut(updateActionBody, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
   });
 
 
   describe('Testing for correct return type (status code 200)', () => {
-    test('Has correct return type for player on Question 1', () => {
+    // Update updateActionBody to QUESTION_OPEN state 
+    updateActionBody = { action: QuizSessionAction.NEXT_QUESTION };
+    requestPut(updateActionBody, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
+    updateActionBody = { action: QuizSessionAction.SKIP_COUNTDOWN };
+    requestPut(updateActionBody, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
+
+    test('Has correct return type for player on QUESTION_OPEN STATE', () => {
       const res = requestGet({}, `/v1/player/${playerId}/question/${questionposition}`);
       expect(res).toStrictEqual({
           "questionId": questionId,
@@ -79,9 +98,9 @@ describe('POST /v1/player/join', () => {
           "points": 5,
           "answers": [
             {
-              "answerId": res.retval.answerId,
+              "answerId": expect.any(Number),
               "answer": "Prince Charles",
-              "colour": res.retval.colour,
+              "colour": expect.any(String),
             }
           ]
       });
@@ -98,9 +117,9 @@ describe('POST /v1/player/join', () => {
           "points": 5,
           "answers": [
             {
-              "answerId": res.retval.answerId,
+              "answerId": expect.any(Number),
               "answer": "Prince Charles",
-              "colour": res.retval.colour,
+              "colour": expect.any(String),
             }
           ]
       });
@@ -108,9 +127,65 @@ describe('POST /v1/player/join', () => {
 
   });
 
+  describe('Testing for error cases (status code 400)', () => {
+    // Update updateActionBody to QUESTION_OPEN state 
+    updateActionBody = { action: QuizSessionAction.NEXT_QUESTION };
+    requestPut(updateActionBody, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
+    updateActionBody = { action: QuizSessionAction.SKIP_COUNTDOWN };
+    requestPut(updateActionBody, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
 
+    test('PlayerId does not exist', () => {
+      const res = requestGet({}, `/v1/player/${playerId + 1}/question/${questionposition}`);
+      expect(res).toStrictEqual({ retval: ERROR, statusCode: 400 });
+    });
+
+    test('Question position is not valid for this session', () => {
+      const res = requestGet({}, `/v1/player/${playerId}/question/${questionposition + 5}`);
+      expect(res).toStrictEqual({ retval: ERROR, statusCode: 400 });
+    });
+
+    test('Session is not currently on this question', () => {
+      const res = requestGet({}, `/v1/player/${playerId}/question/${questionposition + 1}`);
+      expect(res).toStrictEqual({ retval: ERROR, statusCode: 400 });
+    });
+  });
+
+  describe('Testing for session state error cases (status code 400)', () => {
+    test('Session is in LOBBY state', () => {
+      const res = requestGet({}, `/v1/player/${playerId}/question/${questionposition}`);
+      expect(res).toStrictEqual({ retval: ERROR, statusCode: 400 });
+    });
+    test('Session is in QUESTION_COUNTDOWN state', () => {
+      updateActionBody = { action: QuizSessionAction.NEXT_QUESTION };
+
+      requestPut(updateActionBody, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
+      const res = requestGet({}, `/v1/player/${playerId}/question/${questionposition}`);
+      expect(res).toStrictEqual({ retval: ERROR, statusCode: 400 });
+    });
+    test('Session is in FINAL_RESULTS state', () => {
+      updateActionBody = { action: QuizSessionAction.NEXT_QUESTION };
+      requestPut(updateActionBody, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
+      updateActionBody = { action: QuizSessionAction.SKIP_COUNTDOWN };
+      requestPut(updateActionBody, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
+
+      // wait question duration to go to QUESTION_CLOSE
+      const duration = createBody.questionBody.duration;
+      sleepSync(duration * 1000);
+
+      updateActionBody = { action: QuizSessionAction.GO_TO_FINAL_RESULTS };
+      requestPut(updateActionBody, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
+      const res = requestGet({}, `/v1/player/${playerId}/question/${questionposition}`);
+      expect(res).toStrictEqual({ retval: ERROR, statusCode: 400 });
+    });
+    test('Session is in END state', () => {
+      updateActionBody = { action: QuizSessionAction.END};
+      requestPut(updateActionBody, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
+      
+      const res = requestGet({}, `/v1/player/${playerId}/question/${questionposition}`);
+      expect(res).toStrictEqual({ retval: ERROR, statusCode: 400 });
+    });
+  });
 });
-function sleepSync(arg0: number) {
-  throw new Error('Function not implemented.');
-}
+
+
 
