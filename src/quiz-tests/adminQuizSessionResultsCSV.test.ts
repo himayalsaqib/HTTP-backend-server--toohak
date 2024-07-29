5804029eeaf4f75020083c5bb6197188b56076b2
Haleemah Rahman <z5361900@ad.unsw.edu.au>
2024-07-29 21:13:41 +1000

    Added new test for when multiple players join a quiz with multiple questions

--- src/quiz-tests/adminQuizSessionResultsCSV.test.ts
+++ src/quiz-tests/adminQuizSessionResultsCSV.test.ts
@@ -21,6 +21,7 @@ describe('GET /v1/admin/quiz/{quizid}/session/{sessionid}/results/csv', () => {
   let correctAnsIds: number[];
   let incorrectAnsIds: number[];
   let questionPosition: number;
+  let questionPosistion: number;
 
   beforeEach(() => {
     // Registering a user
@@ -36,8 +37,8 @@ describe('GET /v1/admin/quiz/{quizid}/session/{sessionid}/results/csv', () => {
     // Create a question
     questionBody = {
       question: 'Who is your favourite artist\'s favourite artist?',
-      duration: 4,
-      points: 9,
+      duration: 5,
+      points: 10,
       answers: [
         { answer: 'Chappell Roan', correct: true },
         { answer: 'Sabrina Carpenter', correct: false }
@@ -67,8 +68,8 @@ describe('GET /v1/admin/quiz/{quizid}/session/{sessionid}/results/csv', () => {
     playerId = playerJoinRes.retval.playerId;
   });
 
-  describe('Testing successful cases (status code 200)', () => {
-    test('Correctly returns a link', () => {
+  describe.only('Testing successful cases (status code 200)', () => {
+    test('Correctly returns a link with data for 1 question, 1 player', () => {
       // Update state to QUESTION_OPEN
       requestPut({ action: QuizSessionAction.NEXT_QUESTION }, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
       requestPut({ action: QuizSessionAction.SKIP_COUNTDOWN }, `/v1/admin/quiz/${quizId}/session/${sessionId}`, { token });
@@ -82,11 +83,75 @@ describe('GET /v1/admin/quiz/{quizid}/session/{sessionid}/results/csv', () => {
 
       const res = requestGet({}, `/v1/admin/quiz/${quizId}/session/${sessionId}/results/csv`, { token });
       // Expect URL to be a valid CSV URL usging regex
-      console.log('res: ', res);
-      expect(res.retval.url).toMatch(/^http:\/\/localhost:3000\/csv\/.+\.csv$/);
+      expect(res.retval.url).toMatch(/^http:\/\/localhost:3200\/csv\/.+\.csv$/);
       expect(res.statusCode).toBe(200);
       expect(res.retval).toHaveProperty('url');
     });
+
+    test.only('Correctly returns a link with data for muliple question and multiple players', () => {
+      // create a second question
+      questionBody = {
+        question: 'Who is the Monarch of England',
+        duration: 5,
+        points: 10,
+        answers: [
+          { answer: 'Prince Charles', correct: true },
+          { answer: 'Prince William', correct: false }
+        ],
+        thumbnailUrl: 'http://google.com/some/file/path.png'
+      };
+      // get questionId
+     requestPost({ questionBody }, `/v2/admin/quiz/${quizId}/question`, { token });
+
+      // qet answerIds for the correct and incorrect ans
+      const quizInfoRes = requestGet({}, `/v2/admin/quiz/${quizId}`, { token });
+      correctAnsIds.push(quizInfoRes.retval.questions[1].answers[0].answerId);
+      incorrectAnsIds.push(quizInfoRes.retval.questions[1].answers[1].answerId);
+
+      // start new session in LOBBY state
+      sessionStartBody = { autoStartNum: 4 };
+      const sessionRes = requestPost(sessionStartBody, `/v1/admin/quiz/${quizId}/session/start`, { token });
+      const newSessionId = sessionRes.retval.sessionId;
+      questionPosistion = 1;
+
+      // multiple players join
+      playerId = requestPost({ sessionId: newSessionId, name: 'Aelin' }, '/v1/player/join').retval.playerId;
+      const playerId2 = requestPost({ sessionId: newSessionId, name: 'Rowan' }, '/v1/player/join').retval.playerId;
+      const playerId3 = requestPost({ sessionId: newSessionId, name: 'Lysandra' }, '/v1/player/join').retval.playerId;
+
+      // update state to QUESTION_OPEN
+      requestPut({ action: QuizSessionAction.NEXT_QUESTION }, `/v1/admin/quiz/${quizId}/session/${newSessionId}`, { token });
+      requestPut({ action: QuizSessionAction.SKIP_COUNTDOWN }, `/v1/admin/quiz/${quizId}/session/${newSessionId}`, { token });
+
+      // submit answers for first question
+      requestPut({ answerIds: [incorrectAnsIds[0]] }, `/v1/player/${playerId}/question/${questionPosistion}/answer`);
+      requestPut({ answerIds: [correctAnsIds[0]] }, `/v1/player/${playerId2}/question/${questionPosistion}/answer`);
+      requestPut({ answerIds: [correctAnsIds[0]] }, `/v1/player/${playerId3}/question/${questionPosistion}/answer`);
+
+      // move to the next question
+      questionPosistion = 2;
+
+      // update state to QUESTION_OPEN
+      requestPut({ action: QuizSessionAction.GO_TO_ANSWER }, `/v1/admin/quiz/${quizId}/session/${newSessionId}`, { token });
+      requestPut({ action: QuizSessionAction.NEXT_QUESTION }, `/v1/admin/quiz/${quizId}/session/${newSessionId}`, { token });
+      requestPut({ action: QuizSessionAction.SKIP_COUNTDOWN }, `/v1/admin/quiz/${quizId}/session/${newSessionId}`, { token });
+
+      // submit next answers
+      requestPut({ answerIds: [incorrectAnsIds[1]] }, `/v1/player/${playerId}/question/${questionPosistion}/answer`);
+      requestPut({ answerIds: [incorrectAnsIds[1]] }, `/v1/player/${playerId2}/question/${questionPosistion}/answer`);
+      requestPut({ answerIds: [correctAnsIds[1]] }, `/v1/player/${playerId3}/question/${questionPosistion}/answer`);
+
+      // sets state to FINAL_RESULTS
+      requestPut({ action: QuizSessionAction.GO_TO_ANSWER }, `/v1/admin/quiz/${quizId}/session/${newSessionId}`, { token });
+      requestPut({ action: QuizSessionAction.GO_TO_FINAL_RESULTS }, `/v1/admin/quiz/${quizId}/session/${newSessionId}`, { token });
+
+      const res = requestGet({}, `/v1/admin/quiz/${quizId}/session/${newSessionId}/results/csv`, { token });
+      // Expect URL to be a valid CSV URL usging regex
+      expect(res.retval.url).toMatch(/^http:\/\/localhost:3200\/csv\/.+\.csv$/);
+      expect(res.statusCode).toBe(200);
+      expect(res.retval).toHaveProperty('url');
+
+    })
   });
 
   describe('Testing for session errors (status code 400)', () => {
